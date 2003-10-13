35aa81cdcec47ed77d21f8b9fa8f372acec2e114
Lars Lindner <lars.lindner@gmail.com>
2003-10-13 01:01:29 +0000

    blogChannel namespace

---
+++ src/ns_blogChannel.c
@@ -0,0 +1,167 @@
+/*
+   blogChannel namespace support
+   
+   Copyright (C) 2003 Lars Lindner <lars.lindner@gmx.net>
+
+   This program is free software; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 2 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+
+#include "htmlview.h"
+#include "netio.h"
+#include "ns_blogChannel.h"
+
+static gchar ns_bC_prefix[] = "blogChannel";
+
+/* the spec at Userland http://backend.userland.com/blogChannelModule
+
+   blogChannel contains of four channel tags
+   - blogRoll
+   - mySubscriptions
+   - blink
+   - changes	(ignored)
+*/
+
+extern gchar * getOutlineContents(xmlNodePtr cur);	/* opml.c */
+
+gchar * ns_bC_getRSSNsPrefix(void) { return ns_bC_prefix; }
+
+/* simple function to retrieve an OPML document and 
+   parse and output all depth 1 outline tags as
+   HTML into a buffer */
+static gchar * getOutlineList(gchar *url) {
+	xmlDocPtr 	doc = NULL;
+	xmlNodePtr 	cur;
+	gchar		*data, *tmp, *buffer;
+	
+	if(NULL == (data = downloadURL(url)))
+		return NULL;
+
+	buffer = NULL;	/* the following code somewhat duplicates opml.c */
+	while(1) {
+		doc = xmlRecoverMemory(data, strlen(data));
+		g_free(data);
+		
+		if(NULL == doc)
+			break;
+			
+		if(NULL == (cur = xmlDocGetRootElement(doc)))
+			break;
+
+		if(!xmlStrcmp(cur->name, BAD_CAST"opml") ||
+		   !xmlStrcmp(cur->name, BAD_CAST"oml") ||
+		   !xmlStrcmp(cur->name, BAD_CAST"outlineDocument")) {
+		   	// nothing
+		} else
+			break;
+	
+		cur = cur->xmlChildrenNode;
+		while(cur != NULL) {
+			if(!xmlStrcmp(cur->name, BAD_CAST"body")) {
+				/* process all <outline> tags */
+				cur = cur->xmlChildrenNode;
+				while(cur != NULL) {
+					if(!xmlStrcmp(cur->name, BAD_CAST"outline")) {
+						addToHTMLBuffer(&buffer, tmp = getOutlineContents(cur));
+						g_free(tmp);
+					}
+					cur = cur->next;
+				}
+				break;
+			}
+			cur = cur->next;
+		}
+		break;		
+	}
+
+	if(NULL != doc)
+		xmlFreeDoc(doc);
+		
+	return buffer;
+}
+
+static void ns_bC_addInfoStruct(GHashTable *nslist, gchar *tagname, gchar *tagvalue) {
+	GHashTable	*nsvalues;
+	
+	g_assert(nslist != NULL);
+	
+	if(tagvalue == NULL)
+		return;
+			
+	if(NULL == (nsvalues = (GHashTable *)g_hash_table_lookup(nslist, ns_bC_prefix))) {
+		nsvalues = g_hash_table_new(g_str_hash, g_str_equal);
+		g_hash_table_insert(nslist, (gpointer)ns_bC_prefix, (gpointer)nsvalues);
+	}
+	g_hash_table_insert(nsvalues, (gpointer)tagname, (gpointer)tagvalue);
+}
+
+static void ns_bC_parseChannelTag(RSSChannelPtr cp, xmlDocPtr doc, xmlNodePtr cur) {
+	int 		i;
+	gchar		*output;
+
+	if(!xmlStrcmp("blogRoll", cur->name)) {	
+		if(NULL != (output = getOutlineList(xmlNodeListGetString(doc, cur->xmlChildrenNode, 1))))
+			ns_bC_addInfoStruct(cp->nsinfos, "blogRoll", output);
+
+	} else if(!xmlStrcmp("mySubscriptions", cur->name)) {
+		if(NULL != (output = getOutlineList(xmlNodeListGetString(doc, cur->xmlChildrenNode, 1))))
+			ns_bC_addInfoStruct(cp->nsinfos, "mySubscriptions", output);
+
+	} else if(!xmlStrcmp("blink", cur->name)) {
+		ns_bC_addInfoStruct(cp->nsinfos, "blink",  xmlNodeListGetString(doc, cur->xmlChildrenNode, 1));
+	}
+}
+
+/* maybe I should overthink method names :-) */
+static void ns_bC_output(gpointer key, gpointer value, gpointer userdata) {
+	gchar 	**buffer = (gchar **)userdata;
+	
+	addToHTMLBuffer(buffer, (gchar *)value);
+g_print("RSS output: %s\n", value);
+}
+
+static gchar * ns_bC_doOutput(GHashTable *nsinfos) {
+	GHashTable	*nsvalues;
+	gchar		*buffer = NULL;
+	
+	/* we print all channel infos as a (key,value) table */
+	if(NULL != (nsvalues = g_hash_table_lookup(nsinfos, (gpointer)ns_bC_prefix))) {
+		g_hash_table_foreach(nsvalues, ns_bC_output, (gpointer)&buffer);
+	}
+	
+	return buffer;
+}
+
+static gchar * ns_bC_doChannelOutput(gpointer obj) {
+
+	if(NULL != obj)
+		return ns_bC_doOutput(((RSSItemPtr)obj)->nsinfos);
+		
+	return NULL;
+}
+
+RSSNsHandler *ns_bC_getRSSNsHandler(void) {
+	RSSNsHandler 	*nsh;
+	
+	if(NULL != (nsh = (RSSNsHandler *)g_malloc(sizeof(RSSNsHandler)))) {
+		nsh->parseChannelTag		= ns_bC_parseChannelTag;
+		nsh->parseItemTag		= NULL;
+		nsh->doChannelHeaderOutput	= NULL;
+		nsh->doChannelFooterOutput	= NULL;
+		nsh->doItemHeaderOutput		= NULL;
+		nsh->doItemFooterOutput		= ns_bC_doChannelOutput;
+	}
+
+	return nsh;
+}
