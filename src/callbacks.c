31684f3ac6dbbe06e70bccfd8bb69c72fba02747
Lars Lindner <lars.lindner@gmail.com>
2004-01-05 11:38:41 +0000

    folder item counter from Christophe Barbe

--- src/callbacks.c
+++ src/callbacks.c
@@ -1,5 +1,7 @@
 /*
-   Copyright (C) 2003 Lars Lindner <lars.lindner@gmx.net>
+   Copyright (C) 2003, 2004
+   Lars Lindner <lars.lindner@gmx.net>
+   Christophe Barbe <christophe.barbe@ufies.org>	
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
