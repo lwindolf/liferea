cd57691d25283f5bd81a6e0c2f65eef2d607c4f9
Lars Lindner <lars.lindner@gmail.com>
2004-01-21 22:48:12 +0000

    updated to SnowNews 1.4.3 HTTP client

--- src/conversions.c
+++ src/conversions.c
@@ -25,15 +25,311 @@
  */
  
 #include <string.h>
-#include <stdlib.h>
+#include <iconv.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
 #include <zlib.h>
 
 #include "conversions.h"
-#include "callbacks.h"
+#include "config.h"
+#include "interface.h"
+
+#ifdef SUN
+#	include "os-support.h"
+#endif
+
+char * iconvert (char * inbuf, char * from, char * to) {
+	iconv_t cd;							/* Iconvs conversion descriptor. */
+	char *outbuf, *outbuf_first;		/* We need two pointers so we do not lose
+	                                       the strings starting position. */
+	size_t inbytesleft, outbytesleft;
+
+	inbytesleft = strlen(inbuf);
+	outbytesleft = strlen(inbuf);
+	
+	outbuf = malloc (outbytesleft+1);
+	outbuf_first = outbuf;
+	
+	cd = iconv_open (to, from);
+	if (cd == (iconv_t) -1) {
+		return NULL;
+	}
+
+	if (iconv (cd, &inbuf, &inbytesleft, &outbuf, &outbytesleft) == -1) {
+		return NULL;
+	}
+
+	*outbuf = 0;
+	
+	iconv_close (cd);
+	
+	return outbuf_first;
+}
+
+
+/* UIDejunk: remove crap (=html tags) from feed description and convert
+ * html entities to something useful if we hit them.
+ * This function took almost forever to get right, but at least I learned
+ * that html entity &hellip; has nothing to do with Lucifer's ISP, but
+ * instead means "..." (3 dots, "and so on...").
+ */
+char * UIDejunk (char * feed_description) {
+	char *start;			/* Points to first char everytime. Need to free this. */
+	char *text;				/* = feed_description.
+	                           Well, at least at the beginning of the func. */
+	char *newtext;			/* Detag'ed *text. */
+	char *detagged;			/* Returned value from strsep. This is what we want. */
+	char *entity;			/* Which HTML crap did we hit now? */
+	
+	/* Make a copy and point *start to it so we can free the stuff again! */
+	text = strdup (feed_description);
+	start = text;
+	
+	/* First we remove every HTML tag and other junk from the string. */
+	
+	/* Remove spaces from beginning of string. */
+	/*
+	while (1) {
+		if (text[0] == " ")
+			strsep (&text, " ");
+		else
+			break;
+	}
+	*/
+	
+	/* If *text begins with a tag, discard all of them. */
+	while (1) {
+		if (text[0] == '<') {
+			strsep (&text, "<");
+			strsep (&text, ">");
+		} else
+			break;
+		if (text == NULL) {
+			newtext = malloc (strlen (_("No description available."))+1);
+			strcpy (newtext, _("No description available."));
+			free (start);
+			return newtext;
+		}
+	}
+	newtext = malloc (1);
+	newtext[0] = '\0';
+	
+	while (1) {
+		/* Strip tags... tagsoup mode. */
+		/* strsep puts everything before "<" into detagged. */
+		detagged = strsep (&text, "<");
+		if (detagged == NULL)
+			break;
+		
+		/* if (*detagged != '\0') { */
+			newtext = realloc (newtext, strlen(newtext)+strlen(detagged)+1);
+		
+			/* Now append detagged to newtext. */
+			strcat (newtext, detagged);
+		/* } */
+		
+		/* Advance *text to next position after the closed tag. */
+		if ((strsep (&text, ">")) == NULL)
+			break;
+	}
+	free (start);
+	
+	/* See if there are any entities in the string at all. */
+	if (strchr(newtext, '&') != NULL) {
+		text = strdup (newtext);
+		start = text;
+		free (newtext);
+	
+		newtext = malloc (1);
+		newtext[0] = '\0';
+		
+		while (1) {
+			if (text[0] == '&') {
+				strsep (&text, "&");
+				entity = strsep (&text, ";");
+			} else
+			 break;
+		}
+	
+		while (1) {
+			/* Strip HTML entities. */
+			detagged = strsep (&text, "&");
+			if (detagged == NULL) {
+				break;
+				/* This will indeed break if text starts with an HTML entity. */
+			}
+			if (detagged != '\0') {
+				newtext = realloc (newtext, strlen(newtext)+strlen(detagged)+1);
+				strcat (newtext, detagged);
+			}
+			/* Expand newtext by one char. */
+			newtext = realloc (newtext, strlen(newtext)+2);
+			/* This might break if there is an & sign in the text. */
+			entity = strsep (&text, ";");
+			if (entity != NULL) {
+				if (strcmp(entity, "hellip") == 0) {
+					/* "..." needs 3 chars.
+					   WARNING! strlen(newtext) is the same as before!
+					   Make sure to reserve enough space. */
+					newtext = realloc (newtext, strlen(newtext)+4);
+					strcat (newtext, "...");
+				} else if (strcmp(entity, "amp") == 0) {
+					strcat (newtext, "&");
+				} else if (strcmp(entity, "auml") == 0) {
+					strcat (newtext, "ä");
+				} else if (strcmp(entity, "ouml") == 0) {
+					strcat (newtext, "ö");
+				} else if (strcmp(entity, "uuml") == 0) {
+					strcat (newtext, "ü");
+				} else if (strcmp(entity, "Auml") == 0) {
+					strcat (newtext, "Ä");
+				} else if (strcmp(entity, "Ouml") == 0) {
+					strcat (newtext, "Ö");
+				} else if (strcmp(entity, "Uuml") == 0) {
+					strcat (newtext, "Ü");
+				} else if (strcmp(entity, "szlig") == 0) {
+					strcat (newtext, "ß");
+				} else if (strcmp(entity, "lt") == 0) {
+					strcat (newtext, "<");
+				} else if (strcmp(entity, "gt") == 0) {
+					strcat (newtext, ">");
+				} else if (strcmp(entity, "nbsp") == 0) {
+					strcat (newtext, " ");
+				} else {
+					/* If nothing matched, put text back in.
+					   realloc: &+entity+; */
+					/* Changed into &+entity to avoid stray semicolons
+					   at the end of wrapped text if no entity matches. */
+					newtext = realloc (newtext, strlen(newtext)+strlen(entity)+2);
+					strcat (newtext, "&");
+					strcat (newtext, entity);
+					/* strcat (newtext, ";"); */
+				}
+			} else
+				break;
+		}
+		free (start);
+	}	
+	return newtext;
+}
+
+/* 5th try at a wrap text functions.
+ * My first version was broken, my second one sucked, my third try was
+ * so overcomplicated I didn't understand it anymore... Kianga tried
+ * the 4th version which corrupted some random memory unfortunately...
+ * but this one works. Heureka!
+ */
+char * WrapText (char * text, int width) {
+	char *newtext;
+	char *textblob;			/* Working copy of text. */
+	char *chapter;
+	char *line;				/* One line of text with max width. */
+	char *savepos;			/* Saved position pointer so we can go back in the string. */
+	char *chunk;
+	char *start;
+	/*
+	char *p;
+	int lena, lenb;
+	*/
+	
+	textblob = strdup (text);
+	start = textblob;
+	
+
+	line = malloc (1);
+	/* memset (line, 0, width); */	/* Nullify contents of line. */
+	line[0] = '\0';
+	
+	newtext = malloc(1);
+	memset (newtext, 0, 1);
+
+	while (1) {
+		/* First, cut at \n. */
+		chapter = strsep (&textblob, "\n");
+		if (chapter == NULL)
+			break;
+		while (1) {
+			savepos = chapter;
+			chunk = strsep (&chapter, " ");
+			
+			/* Last chunk. */
+			if (chunk == NULL) {
+				if (line != NULL) {
+					newtext = realloc (newtext, strlen(newtext)+strlen(line)+2);
+					strcat (newtext, line);
+					strcat (newtext, "\n");
+					/* Faster replacement with memcpy, */
+					/*
+					lena = strlen(newtext);
+					lenb = strlen(line);
+					newtext = realloc (newtext, lena+lenb+2);
+					p = newtext+lena;
+					memcpy (p, line, lenb);
+					p += lenb;
+					*p = '\n';
+					p++;
+					*p=0;
+					*/
+					
+					line[0] = '\0';
+				}
+				break;
+			}
+			
+			if (strlen(chunk) > width) {
+				/* First copy remaining stuff in line to newtext. */
+				newtext = realloc (newtext, strlen(newtext)+strlen(line)+2);
+				strcat (newtext, line);
+				strcat (newtext, "\n");
+				
+				free (line);
+				line = malloc (1);
+				line[0] = '\0';
+				
+				/* Then copy chunk with max length of line to newtext. */
+				line = realloc (line, width+1);
+				strncat (line, chunk, width-5);
+				strcat (line, "...");
+				newtext = realloc (newtext, strlen(newtext)+width+2);
+				strcat (newtext, line);
+				strcat (newtext, "\n");
+				free (line);
+				line = malloc (1);
+				line[0] = '\0';
+				continue;
+			}
+
+			if (strlen(line)+strlen(chunk) <= width) {
+				line = realloc (line, strlen(line)+strlen(chunk)+2);
+				strcat (line, chunk);
+				strcat (line, " ");
+			} else {
+				/* Why the fuck can chapter be NULL here anyway? */
+				if (chapter != NULL) {
+					chapter--;
+					chapter[0] = ' ';
+				}
+				chapter = savepos;
+				newtext = realloc (newtext, strlen(newtext)+strlen(line)+2);
+				strcat (newtext, line);
+				strcat (newtext, "\n");
+				free (line);
+				line = malloc (1);
+				line[0] = '\0';
+			}
+		}
+	}
+	
+	if (line != NULL)
+		free (line);
+	
+	free (start);	
+	
+	return newtext;
+}
 
 void *zlib_uncompress(void *in_buf, int in_size, int *out_size, int voodoo_magic) {
 	char tmpstring[1024];
@@ -149,4 +445,61 @@ void *gzip_uncompress(void *in_buf, int in_size, int *out_size) {
 	data_start = (char *)in_buf + offset;
 	
 	return zlib_uncompress(data_start, in_size - offset - 8, out_size, 1);
+}
+
+
+char *base64encode(char const *inbuf, unsigned int inbuf_size) {
+	static unsigned char const alphabet[64] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+	char *outbuf = NULL;
+	unsigned int inbuf_pos = 0;
+	unsigned int outbuf_pos = 0;
+	unsigned int outbuf_size = 0;
+	int bits = 0;
+	int char_count = 0;
+	
+	outbuf = malloc(1);
+	
+	while (inbuf_pos < inbuf_size) {
+	
+		bits |= *inbuf;
+		char_count++;
+		
+		if (char_count == 3) {
+			outbuf = realloc(outbuf, outbuf_size+4);
+			outbuf_size += 4;
+			outbuf[outbuf_pos+0] = alphabet[bits >> 18];
+			outbuf[outbuf_pos+1] = alphabet[(bits >> 12) & 0x3f];
+			outbuf[outbuf_pos+2] = alphabet[(bits >> 6) & 0x3f];
+			outbuf[outbuf_pos+3] = alphabet[bits & 0x3f];
+			outbuf_pos += 4;
+			bits = 0;
+			char_count = 0;
+		}
+		
+		inbuf++;
+		inbuf_pos++;
+		bits <<= 8;
+	}
+	
+	if (char_count > 0) {
+		bits <<= 16 - (8 * char_count);
+		outbuf = realloc(outbuf, outbuf_size+4);
+		outbuf_size += 4;
+		outbuf[outbuf_pos+0] = alphabet[bits >> 18];
+		outbuf[outbuf_pos+1] = alphabet[(bits >> 12) & 0x3f];
+		if (char_count == 1) {
+			outbuf[outbuf_pos+2] = '=';
+			outbuf[outbuf_pos+3] = '=';
+		} else {
+			outbuf[outbuf_pos+2] = alphabet[(bits >> 6) & 0x3f];
+			outbuf[outbuf_pos+3] = '=';
+		}
+		outbuf_pos += 4;
+	}
+	
+	outbuf = realloc(outbuf, outbuf_size+1);
+	outbuf[outbuf_pos] = 0;
+	
+	return outbuf;
 }
--- src/netio.c
+++ src/netio.c
@@ -34,29 +34,22 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
-#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h>
 #include <sys/stat.h>
-#include <conversions.h>
 
-#include <libxml/xmlmemory.h>
-#include <libxml/parser.h>
-
-/*-----------------------------------------------------------------------*/
-/* some Liferea specific adaptions					 */
-
-#include "callbacks.h"
-#include "support.h"
-#include "netio.h"
-
-/*-----------------------------------------------------------------------*/
+#include "conversions.h"
 
 #ifdef SUN
 #	include "os-support.h"
 #endif
+
+/* some Liferea specific includes */
+#include "callbacks.h"
+#include "support.h"
+#include "netio.h"
 
 #define MAX_HTTP_REDIRECTS 10		/* Maximum number of redirects we will follow. */
 
@@ -72,13 +65,12 @@ int connectresult;
  *			 3	couldn't connect
  *                 	-1	aborted by user
  */
-int NetConnect (int * my_socket, char * host, int httpproto) {
+int NetConnect (int * my_socket, char * host, int httpproto, int suppressoutput) {
+	int retval;
 	struct sockaddr_in address;	
 	struct hostent *remotehost;
 	char *uistring;
 	int uistringlength;
-	fd_set rfdsr;
-	fd_set rfdsw;
 	socklen_t len;
 	char *realhost;
 	unsigned short port;
@@ -91,7 +83,8 @@ int NetConnect (int * my_socket, char * host, int httpproto) {
 	uistringlength = strlen(_("Connecting to ")) + strlen(host) + 4;
 	uistring = malloc (uistringlength);
 	snprintf (uistring, uistringlength, _("Connecting to %s..."), host);
-	UIStatus (uistring, 0);
+	if (!suppressoutput)
+		UIStatus (uistring, 0);
 	free (uistring);
 	
 	/* Create a inet stream TCP socket. */
@@ -99,14 +92,6 @@ int NetConnect (int * my_socket, char * host, int httpproto) {
 	if (*my_socket == -1) {
 		return 1;
 	}
-	
-	/* Set socket to nonblock mode to make it possible to interrupt the connect. */
-	//fcntl(my_socket, F_SETFL, fcntl(my_socket, F_GETFL, 0) | O_NONBLOCK);
-	
-	/* stdin is read, socket is write, so we need read/write sets. */
-	FD_ZERO(&rfdsr);
-	FD_ZERO(&rfdsw);
-	FD_SET(*my_socket, &rfdsw);
 	
 	/* If proxyport is 0 we didn't execute the if http_proxy statement in main
 	   so there is no proxy. On any other value of proxyport do proxyrequests instead. */
@@ -184,18 +169,23 @@ int NetConnect (int * my_socket, char * host, int httpproto) {
 		}
 	}
 	
-	/* Set socket back to blocking mode. */
-	//fcntl(my_socket, F_SETFL, fcntl(my_socket, F_GETFL, 0) & ~O_NONBLOCK);
-	
 	free (realhost);
 	return 0;
 }
 
 
 /*
- * Kiza's crufty HTTP client hack.
+ * Main network function.
+ * (Now with a useful function description *g*)
+ *
+ * This function returns the HTTP request's body (deflating gzip encoded data
+ * if needed).
+ * Updates passed feed struct with values gathered from webserver.
+ * Handles all redirection and HTTP status decoding.
+ * Returns NULL pointer if no data was received. Check httpstatus == 304,
+ * otherwise an error occured.
  */
-char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cur_ptr, int httpproto) {
+char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cur_ptr, int httpproto, char * authinfo, int suppressoutput) {
 	char netbuf[4096];			/* Network read buffer. */
 	char *body;					/* XML body. */
 	int length;
@@ -213,7 +203,6 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 	char *tmpstring;			/* Temp pointers. */
 	char *freeme;
 	char *redirecttarget;
-	fd_set rfds;
 	int retval;
 	int handled;
 	int tmphttpstatus;
@@ -224,7 +213,8 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 	int quirksmode = 0;
 	
 	snprintf (tmp, sizeof(tmp), _("Downloading http://%s%s..."), host, url);
-	UIStatus (tmp, 0);
+	if (!suppressoutput)
+		UIStatus (tmp, 0);
 
 	redirectcount = 0;
 	
@@ -246,10 +236,10 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 	if (proxyport == 0) {
 		/* Request URL from HTTP server. */
 		if (cur_ptr->lastmodified != NULL)
-			fprintf(stream, "GET %s HTTP/1.0\r\nAccept-Encoding: gzip,deflate\r\nUser-Agent: %s\r\nConnection: close\r\nHost: %s\r\nIf-Modified-Since: %s\r\n\r\n", url, useragent, host, cur_ptr->lastmodified);
+			fprintf(stream, "GET %s HTTP/1.0\r\nAccept-Encoding: gzip,deflate\r\nUser-Agent: %s\r\nConnection: close\r\nHost: %s\r\nIf-Modified-Since: %s\r\n%s\r\n", url, useragent, host, cur_ptr->lastmodified, (authinfo ? authinfo : ""));
 		else
-			fprintf(stream, "GET %s HTTP/1.0\r\nAccept-Encoding: gzip,deflate\r\nUser-Agent: %s\r\nConnection: close\r\nHost: %s\r\n\r\n", url, useragent, host);
-		
+			fprintf(stream, "GET %s HTTP/1.0\r\nAccept-Encoding: gzip,deflate\r\nUser-Agent: %s\r\nConnection: close\r\nHost: %s\r\n%s\r\n", url, useragent, host, (authinfo ? authinfo : ""));
+
 		fflush(stream);
 	} else {
 		/* Request URL from HTTP server. */
@@ -261,10 +251,6 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 		fflush(stream);
 	}
 	
-	/* Use select to make the connection interuptable if it should hang. */
-	FD_ZERO(&rfds);
-	FD_SET(*my_socket, &rfds);
-
 	if ((fgets (tmp, sizeof(tmp), stream)) == NULL) {
 		fclose (stream);
 		return NULL;
@@ -315,7 +301,8 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 				/* Give up if we reach MAX_HTTP_REDIRECTS to avoid loops. */
 				if (redirectcount > MAX_HTTP_REDIRECTS) {
 					fclose (stream);
-					UIStatus (_("Too many HTTP redirects encountered! Giving up."), 2);
+					if (!suppressoutput)
+						UIStatus (_("Too many HTTP redirects encountered! Giving up."), 2);
 					return NULL;
 				}
 				
@@ -342,6 +329,7 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 						   after the word "Location:" */
 						strsep (&redirecttarget, " ");
 						
+						/* Location must start with "http", otherwise switch on quirksmode. */
 						if (strncmp(redirecttarget, "http", 4) != 0)
 							quirksmode = 1;
 						
@@ -367,7 +355,8 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 						
 						/* Change cur_ptr->feedurl on 301. */
 						if (cur_ptr->lasthttpstatus == 301) {
-							UIStatus (_("URL points to permanent redirect, updating with new location..."), 2);
+							if (!suppressoutput)
+								UIStatus (_("URL points to permanent redirect, updating with new location..."), 2);
 							free (cur_ptr->feedurl);
 							cur_ptr->feedurl = strdup (newlocation);
 						}
@@ -378,15 +367,23 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 						tmphost = newlocation;
 						/* The following line \0-terminates tmphost in overwriting the first
 						   / after the hostname. */
-						if(strchr(newlocation, '/')) {
-							strsep (&newlocation, "/");
-					
-							newhost = strdup (tmphost);
-							newlocation--;
-							newlocation[0] = '/';
-						} else {
-							newhost = strdup (tmphost);
+						strsep (&newlocation, "/");
+						
+						/* newlocation must now be the absolute path on newhost.
+						   If not we've been redirected to somewhere totally stupid
+						   (oh yeah, no offsite linking, go to our fucking front page).
+						   Say goodbye to the webserver in this case. In fact, we don't
+						   even say goodbye, but just drop the connection. */
+						if (newlocation == NULL) {
+							if (!suppressoutput)
+								UIStatus (_("Webserver sent an invalid redirect!"), 2);
+							fclose (stream);
+							return NULL;
 						}
+						
+						newhost = strdup (tmphost);
+						newlocation--;
+						newlocation[0] = '/';
 						newurl = strdup (newlocation);
 					
 						free (freeme);
@@ -395,7 +392,7 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 						fclose (stream);
 					
 						/* Reconnect to server. */
-						if ((NetConnect (my_socket, newhost, httpproto)) != 0) {
+						if ((NetConnect (my_socket, newhost, httpproto, suppressoutput)) != 0) {
 							/* Add error handling/reporting. */
 							return NULL;
 						}
@@ -411,10 +408,13 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 				/* Not modified received. We can close stream and return from here.
 				   Not very friendly though. :) */
 				fclose (stream);
+				/* Received good status from server, clear problem field. */
+				cur_ptr->problem = 0;
 				return NULL;
 			case 410: /* The feed is gone. Politely remind the user to unsubscribe. */
 				snprintf (tmp, sizeof(tmp), _("The feed no longer exists. Please unsubscribe!"));
-				UIStatus (tmp, 3);
+				if (!suppressoutput)
+					UIStatus (tmp, 3);
 			case 400:
 				cur_ptr->problem = 1;
 				fclose (stream);
@@ -437,10 +437,6 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 	/* Read rest of HTTP header and parse what we need. */
 	while (!feof(stream)) {
 	
-		/* Use select to make the connection interuptable if it should hang. */
-		FD_ZERO(&rfds);
-		FD_SET(*my_socket, &rfds);
-	
 		/* Max line length of sizeof(netbuf) is assumed here.
 		   If header has longer lines than 4096 bytes something may go wrong. :) */
 		if ((fgets (netbuf, sizeof(netbuf), stream)) == NULL)
@@ -451,7 +447,8 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 			   Chunked encoding is not defined in HTTP/1.0. */
 			chunked = 1;
 			snprintf (tmp, sizeof(tmp), _("The webserver %s sent illegal HTTP/1.0 reply! I cannot parse this."), host);
-			UIStatus (tmp, 2);
+			if (!suppressoutput)
+				UIStatus (tmp, 2);
 			cur_ptr->problem = 1;
 			fclose (stream);
 			return NULL;
@@ -510,10 +507,6 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 
 	/* Read stream until EOF and return it to parent. */
 	while (!feof(stream)) {
-	
-		/* Use select to make the connection interuptable if it should hang. */
-		FD_ZERO(&rfds);
-		FD_SET(*my_socket, &rfds);
 
 		/* Since we handle binary data if we read compressed input we
 		   need to use fread instead of fgets after reading the header. */ 
@@ -564,33 +557,38 @@ char * NetIO (int * my_socket, char * host, char * url, struct feed_request * cu
 	return body;
 }
 
-char * DownloadFeed (char * url, struct feed_request * cur_ptr) {
+/* Returns allocated string with body of webserver reply.
+   Various status info put into struct feed * cur_ptr.
+   Set suppressoutput=1 to disable ncurses calls. */
+char * DownloadFeed (char * url, struct feed_request * cur_ptr, int suppressoutput) {
 	int my_socket = 0;
 	int result;
+	int len;
 	char *host;					/* Needs to freed. */
 	char *tmphost;
 	char *freeme;
 	char *returndata;
+	char *authinfo = NULL;		/* base64 encoded HTTP auth. Default to NULL! */
+	char *authstring;			/* "username:password" - to be base64 encoded */
+	char *username, *password;	/* plaintext username/password for HTTP auth */
+	char *tmpstr;
 	char tmp[1024];
-	char httpprotostr[6];		/* http or https */
 	int httpproto = 0;			/* 0: http; 1: https */
-
-	strncpy (httpprotostr, url, 5);
-	httpprotostr[5] = 0;
-	if (strstr (httpprotostr, "https") != NULL)
+	
+	/* strstr will match _any_ substring. Not good, use strncasecmp with length 5! */
+	if (strncasecmp (url, "https", 5) == 0)
 		httpproto = 1;
 	else
 		httpproto = 0;
 	
-	/* hahteeteepeh://foo.bar/jaaaaaguar.html. */
-	/*              00^      0^*/
+	/* hahteeteepeh://foo.bar/jaaaaaguar.html.
+	                00^      0^
 	
-	/* Warning!
+	   Warning!
 	   This "function" will not handle the following URLs:
 	   http://foo.bar/
 	   
-	   Must add index.rdf or whatever.
-	*/
+	   Must add index.rdf or whatever. */
 	strsep (&url, "/");
 	strsep (&url, "/");
 	tmphost = url;
@@ -599,10 +597,47 @@ char * DownloadFeed (char * url, struct feed_request * cur_ptr) {
 		/* End of string url. Probably entered invalid stuff like
 		   http://asdf or something. Since we don't want to crash and burn
 		   return the dreaded NULL pointer. */
-		UIStatus (_("This URL doesn't look too valid to me, don't you think?"), 2);
+		if (!suppressoutput)
+			UIStatus (_("This URL doesn't look too valid to me, don't you think?"), 2);
 		return NULL;
 	}
-	/* url[0] = '\0'; */
+		
+	/* If tmphost contains an '@' we have to do HTTP authentication. */
+	if (strchr (tmphost, '@') != NULL) {
+		tmpstr = tmphost;
+		strsep (&tmphost, ":");
+		username = strdup (tmpstr);
+		tmpstr = tmphost;
+		strsep (&tmphost, "@");
+		password = strdup (tmpstr);
+		
+		/* Create base64 authinfo.
+		
+		   RFC 2617. Basic HTTP authentication.
+		   Authorization: Basic username:password[base64 encoded] */
+		
+		/* Construct the cleartext authstring. */
+		len = strlen(username) + 1 + strlen(password) + 1;
+		authstring = malloc (len);
+		strcpy (authstring, username);
+		strcat (authstring, ":");
+		strcat (authstring, password);
+		
+		tmpstr = base64encode (authstring, len);
+		
+		/* "Authorization: Basic " + base64str + \r\n\0 */
+		len = 21 + strlen(tmpstr) + 3;
+		authinfo = malloc (len);
+		strcpy (authinfo, "Authorization: Basic ");
+		strcat (authinfo, tmpstr);
+		strcat (authinfo, "\r\n");
+		
+		free (username);
+		free (password);
+		free (tmpstr);
+		free (authstring);
+	}
+	
 	host = strdup (tmphost);
 	
 	/* netio() might change pointer of host to something else if redirect
@@ -615,38 +650,45 @@ char * DownloadFeed (char * url, struct feed_request * cur_ptr) {
 		url[strlen(url)-1] = '\0';
 	}
 	
-	result = NetConnect (&my_socket, host, httpproto);
+	result = NetConnect (&my_socket, host, httpproto, suppressoutput);
 	
 	switch (result) {
 		case 1:
-			UIStatus (_("Couldn't create network socket!"), 2);
+			if (!suppressoutput)
+				UIStatus (_("Couldn't create network socket!"), 2);
 			cur_ptr->problem = 1;
 			free (freeme);
 			return NULL;
 		case 2:
 			snprintf (tmp, sizeof(tmp), _("Can't resolve host %s!"), host);
-			UIStatus (tmp, 2);
+			if (!suppressoutput)
+				UIStatus (tmp, 2);
 			cur_ptr->problem = 1;
 			free (freeme);
 			return NULL;
 		case 3:
 			snprintf (tmp, sizeof(tmp), _("Could not connect to server %s: %s"), host, strerror(connectresult));
-			UIStatus (tmp, 2);
+			if (!suppressoutput)
+				UIStatus (tmp, 2);
 			cur_ptr->problem = 1;
 			free (freeme);
 			return NULL;
 		case -1:
-			UIStatus (_("Aborted."), 2);
+			if (!suppressoutput)
+				UIStatus (_("Aborted."), 2);
 			free (freeme);
 			return NULL;
 		default:
 			break;
 	}
 	
-	returndata = NetIO (&my_socket, host, url, cur_ptr, httpproto);
+	returndata = NetIO (&my_socket, host, url, cur_ptr, httpproto, authinfo, suppressoutput);
 	
 	/* url will be freed in the calling function. */
 	free (freeme);		/* This is *host. */
+	
+	if (authinfo != NULL)
+		free (authinfo);
 	
 	return returndata;
 }
@@ -670,7 +712,7 @@ char * downloadURL(struct feed_request *request) {
 	if(NULL != strstr(request->feedurl, "://")) {
 		/* :// means it an URL */
 		tmpurl = g_strdup(request->feedurl);	/* necessary because DownloadFeed() works on the URL string */
-		data = DownloadFeed(tmpurl, request);
+		data = DownloadFeed(tmpurl, request, 0);
 		g_free(tmpurl);
 	} else {
 		/* fake a status */
