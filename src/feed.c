95d90a2f2b899afae7234a5b9224883287874cb9
Lars Lindner <lars.lindner@gmail.com>
2004-03-13 13:06:11 +0000

    patch 915419 from Nathan J. Conrad

--- src/feed.c
+++ src/feed.c
@@ -96,7 +96,7 @@ static gint autoDetectFeedType(gchar *url, gchar **data) {
 	g_assert(NULL != pattern);
 	g_assert(NULL != url);
 	
-	request = getNewFeedRequest(NULL);
+	request = getNewRequestStruct(NULL);
 	request->feedurl = g_strdup(url);
 	request->lastmodified = NULL;
 	downloadURL(request);
