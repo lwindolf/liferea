b9751d20adfe35cf2569a94b765bca67024eeff8
Nathan J. Conrad <t98502@users.sourceforge.net>
2004-06-13 06:02:32 +0000

    * Remove debugging statement I accidently left in previous commit * Make feed_get_title always return something and never crash * Remove cause of warning that happens when a feed is added that     can't be downloaded * A line of the auto-discovery code looked quite redundant and was thus   removed (Please tell me if it is really needed)

--- src/feed.c
+++ src/feed.c
@@ -139,9 +139,8 @@ feedHandlerPtr feed_parse(feedPtr fp, gchar *data, gboolean autodiscover) {
 	if(!handled) {
 		/* test if we have a HTML page */
 		if(autodiscover && 
-		   ((NULL != strstr(data, "<html>")) || (NULL != strstr(data, "<HTML>")) || 
-		    (NULL != strstr(data, "<html ")) || (NULL != strstr(data, "<HTML "))
-		   )) {
+		   ((NULL != strstr(data, "<html ")) || (NULL != strstr(data, "<HTML ")))
+		  ) {
 			/* if yes we should scan for links */
 			debug1(DEBUG_UPDATE, "HTML detected, starting feed auto discovery (%s)", feed_get_source(fp));
 			if(NULL != (source = html_auto_discover_feed(data))) {			
@@ -862,8 +861,10 @@ const gchar * feed_get_title(feedPtr fp) {
 
 	if(NULL != fp->title)
 		return fp->title; 
-	else
+	else if (NULL != fp->source)
 		return fp->source;
+	else
+		return NULL;
 }
 
 void feed_set_title(feedPtr fp, const gchar *title) {
