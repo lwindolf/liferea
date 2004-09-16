2a57d535257a48ff3993f2d1a841bc64dc33ac6c
Nathan J. Conrad <t98502@users.sourceforge.net>
2004-09-16 21:08:24 +0000

    This should fix autogen.sh and remove a few scary autoheader warnings (by removing acconfig.h).

--- autogen.sh
+++ autogen.sh
@@ -136,7 +136,7 @@ do
       fi
       echo "Running aclocal $aclocalinclude ..."
       aclocal $aclocalinclude
-      if grep "^AM_CONFIG_HEADER" configure.ac >/dev/null; then
+      if grep "^AC_CONFIG_HEADERS" configure.ac >/dev/null; then
 	echo "Running autoheader..."
 	autoheader
       fi
