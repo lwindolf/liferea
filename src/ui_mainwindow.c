64d39b443e7100c1247d6abfdf8af6e5b5c2c81c
Lars Lindner <lars.lindner@gmail.com>
2004-10-02 23:01:10 +0000

    some user motivation in the welcome message

--- src/ui_mainwindow.c
+++ src/ui_mainwindow.c
@@ -185,9 +185,13 @@ void ui_mainwindow_finish(GtkWidget *window) {
 	/* create welcome text */
 	ui_htmlview_start_output(&buffer, FALSE);
 	addToHTMLBuffer(&buffer, _("<div style=\"background-color:#f7f0a3;padding:5px;border:solid 1px black\">"
-	                           "We started gathering requirements for 1.1. Please have a <a href=\"http://liferea.sourceforge.net/dokuwiki/doku.php?id=programrequirements1.1\">look</a> and "
-				   "make <a href=\"mailto:liferea-devel@lists.sourceforge.net\">proposals</a> if you miss features.</div>"
-	                           "<h2>Welcome to Liferea</h2>"
+	                           "When Liferea starts for the first time it loads a localized default "
+				   "feed list. There are already default lists for English, French, German "
+				   "and Spanish. If you think you know a good set of default feeds please post "
+				   "it (maybe as OPML) on the <a href=\"mailto:liferea-devel@lists.sourceforge.net\">mailing list</a> "
+				   "and it will be included in the next release!</div>"));
+				   
+	addToHTMLBuffer(&buffer, _("<h2>Welcome to Liferea</h2>"
 	                           "<p>The left pane contains the feed list where you can add new subscriptions "
 	                           "and select subscriptions to read their headlines. The right side either "
 	                           "displays a list of the headlines of the selected subscription and a "
