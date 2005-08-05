c5dc53f3b01ebe2e4f96da65082fb7130c72a538
Lars Lindner <lars.lindner@gmail.com>
2005-08-05 08:36:23 +0000

    accidently disabled atom 1.0

--- src/feed.c
+++ src/feed.c
@@ -72,7 +72,7 @@ void feed_init(void) {
 	feedhandlers = g_slist_append(feedhandlers, ocs_init_feed_handler()); /* Must come before RSS/RDF */
 	feedhandlers = g_slist_append(feedhandlers, rss_init_feed_handler());
 	feedhandlers = g_slist_append(feedhandlers, cdf_init_feed_handler());
-/*	feedhandlers = g_slist_append(feedhandlers, atom10_init_feed_handler()); */ /* Must be before pie */
+	feedhandlers = g_slist_append(feedhandlers, atom10_init_feed_handler());  /* Must be before pie */
 	feedhandlers = g_slist_append(feedhandlers, pie_init_feed_handler());
 	feedhandlers = g_slist_append(feedhandlers, opml_init_feed_handler());
 	feedhandlers = g_slist_append(feedhandlers, vfolder_init_feed_handler());
