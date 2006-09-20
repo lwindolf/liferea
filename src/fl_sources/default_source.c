e19751fadcc4f751abbe38fb511aa138f92c653f
Lars Lindner <lars.lindner@gmail.com>
2006-09-20 11:09:56 +0000

    fixing problem in DBUS methods

--- src/fl_sources/default_source.c
+++ src/fl_sources/default_source.c
@@ -42,114 +42,6 @@
 /** lock to prevent feed list saving while loading */
 static gboolean feedlistImport = FALSE;
 
-static void default_source_copy_dir(gchar *subdir) {
-	gchar *dirname10, *dirname11;
-	gchar *srcfile, *destfile;
-   	GDir *dir;
-		
-	dirname10 = g_strdup_printf("%s" G_DIR_SEPARATOR_S ".liferea"     G_DIR_SEPARATOR_S "%s", g_get_home_dir(), subdir);
-	dirname11 = g_strdup_printf("%s" G_DIR_SEPARATOR_S ".liferea_1.1" G_DIR_SEPARATOR_S "%s", g_get_home_dir(), subdir);
-	dir = g_dir_open(dirname10, 0, NULL);
-	while(NULL != (srcfile = (gchar *)g_dir_read_name(dir))) {
-		gchar	*content;
-		gsize	length;
-		destfile = g_strdup_printf("%s" G_DIR_SEPARATOR_S "%s", dirname11, srcfile);
-		srcfile = g_strdup_printf("%s" G_DIR_SEPARATOR_S "%s", dirname10, srcfile);
-		g_print("copying %s to %s\n", srcfile, destfile);
-		if(g_file_get_contents(srcfile, &content, &length, NULL))
-			g_file_set_contents(destfile, content, length, NULL);
-		g_free(content);
-		g_free(destfile);
-		g_free(srcfile);
-	}
-	g_dir_close(dir);
-}
-
-static gchar * default_source_source_get_feedlist(nodePtr node) {
-
-	return common_create_cache_filename(NULL, "feedlist", "opml");
-}
-
-static void default_source_source_import(nodePtr node) {
-	gchar		*filename10, *filename11;
-
-	debug_enter("default_source_source_import");
-
-	/* start the import */
-	feedlistImport = TRUE;
-
-	/* check for 1.0->1.1 migration */
-	filename10 = g_strdup_printf("%s" G_DIR_SEPARATOR_S ".liferea/feedlist.opml", g_get_home_dir()); /* 1.0 path dir */
-	filename11 = default_source_source_get_feedlist(node);
-		
-	if(!g_file_test(filename11, G_FILE_TEST_EXISTS) &&
-	   g_file_test(filename10, G_FILE_TEST_EXISTS)) {
-		
-		g_print("starting 1.0->1.1 cache migration...\n");
-		
-		/* Note: because v1.1 uses a new cache format we
-		   do a cache migration. v1.1 uses $HOME/.liferea_1.1
-		   instead of $HOME/.liferea as it's cache directory */
-
-		/* copy old cache files to new cache dir */
-		default_source_copy_dir("cache" G_DIR_SEPARATOR_S "feeds");
-		default_source_copy_dir("cache" G_DIR_SEPARATOR_S "favicons");
-		
-		/* point feedlist.opml to the old 1.0 file */
-		g_free(filename11);
-		filename11 = g_strdup(filename10);
-	}
-	g_free(filename10);
-
-	/* check for default feed list import */
-	if(!g_file_test(filename11, G_FILE_TEST_EXISTS)) {
-		/* if there is no feedlist.opml we provide a default feed list */
-		g_free(filename11);
-		/* "feedlist.opml" is translatable so that translators can provide a localized default feed list */
-		filename11 = g_strdup_printf(PACKAGE_DATA_DIR G_DIR_SEPARATOR_S PACKAGE G_DIR_SEPARATOR_S "opml" G_DIR_SEPARATOR_S "%s", _("feedlist.opml"));
-	}
-	if(!import_OPML_feedlist(filename11, node, node->source, FALSE, TRUE))
-		g_error("Fatal: Feed list import failed!");
-	g_free(filename11);
-	feedlistImport = FALSE;
-
-#ifdef USE_DBUS
-	if(!getBooleanConfValue(DISABLE_DBUS)) {
-		/* Start listening on the dbus for new subscriptions */	
-		debug0(DEBUG_GUI, "Registering with DBUS...");
-		default_source_dbus_connect();
-	} else {
-		g_print("DBUS disabled by user request...");
-	}
-#else
-	debug0(DEBUG_GUI, "Compiled without DBUS support.");
-#endif
-
-	debug_exit("default_source_source_import");
-}
-
-static void default_source_source_export(nodePtr node) {
-	gchar	*filename;
-	
-	if(feedlistImport)
-		return;
-
-	debug_enter("default_source_source_export");
-	
-	g_assert(node->source->root == feedlist_get_root());
-
-	filename = default_source_source_get_feedlist(node);
-	export_OPML_feedlist(filename, node->source->root, TRUE);
-	g_free(filename);
-
-	debug_exit("default_source_source_export");
-}
-
-static void default_source_source_auto_update(nodePtr node) {
-
-	node_foreach_child(node, node_request_auto_update);
-}
-
 /* DBUS support for new subscriptions */
 
 #ifdef USE_DBUS
@@ -289,6 +181,114 @@ default_source_dbus_message_handler (DBusConnection *connection, DBusMessage *me
 }
 
 #endif /* USE_DBUS */
+
+static void default_source_copy_dir(gchar *subdir) {
+	gchar *dirname10, *dirname11;
+	gchar *srcfile, *destfile;
+   	GDir *dir;
+		
+	dirname10 = g_strdup_printf("%s" G_DIR_SEPARATOR_S ".liferea"     G_DIR_SEPARATOR_S "%s", g_get_home_dir(), subdir);
+	dirname11 = g_strdup_printf("%s" G_DIR_SEPARATOR_S ".liferea_1.1" G_DIR_SEPARATOR_S "%s", g_get_home_dir(), subdir);
+	dir = g_dir_open(dirname10, 0, NULL);
+	while(NULL != (srcfile = (gchar *)g_dir_read_name(dir))) {
+		gchar	*content;
+		gsize	length;
+		destfile = g_strdup_printf("%s" G_DIR_SEPARATOR_S "%s", dirname11, srcfile);
+		srcfile = g_strdup_printf("%s" G_DIR_SEPARATOR_S "%s", dirname10, srcfile);
+		g_print("copying %s to %s\n", srcfile, destfile);
+		if(g_file_get_contents(srcfile, &content, &length, NULL))
+			g_file_set_contents(destfile, content, length, NULL);
+		g_free(content);
+		g_free(destfile);
+		g_free(srcfile);
+	}
+	g_dir_close(dir);
+}
+
+static gchar * default_source_source_get_feedlist(nodePtr node) {
+
+	return common_create_cache_filename(NULL, "feedlist", "opml");
+}
+
+static void default_source_source_import(nodePtr node) {
+	gchar		*filename10, *filename11;
+
+	debug_enter("default_source_source_import");
+
+	/* start the import */
+	feedlistImport = TRUE;
+
+	/* check for 1.0->1.1 migration */
+	filename10 = g_strdup_printf("%s" G_DIR_SEPARATOR_S ".liferea/feedlist.opml", g_get_home_dir()); /* 1.0 path dir */
+	filename11 = default_source_source_get_feedlist(node);
+		
+	if(!g_file_test(filename11, G_FILE_TEST_EXISTS) &&
+	   g_file_test(filename10, G_FILE_TEST_EXISTS)) {
+		
+		g_print("starting 1.0->1.1 cache migration...\n");
+		
+		/* Note: because v1.1 uses a new cache format we
+		   do a cache migration. v1.1 uses $HOME/.liferea_1.1
+		   instead of $HOME/.liferea as it's cache directory */
+
+		/* copy old cache files to new cache dir */
+		default_source_copy_dir("cache" G_DIR_SEPARATOR_S "feeds");
+		default_source_copy_dir("cache" G_DIR_SEPARATOR_S "favicons");
+		
+		/* point feedlist.opml to the old 1.0 file */
+		g_free(filename11);
+		filename11 = g_strdup(filename10);
+	}
+	g_free(filename10);
+
+	/* check for default feed list import */
+	if(!g_file_test(filename11, G_FILE_TEST_EXISTS)) {
+		/* if there is no feedlist.opml we provide a default feed list */
+		g_free(filename11);
+		/* "feedlist.opml" is translatable so that translators can provide a localized default feed list */
+		filename11 = g_strdup_printf(PACKAGE_DATA_DIR G_DIR_SEPARATOR_S PACKAGE G_DIR_SEPARATOR_S "opml" G_DIR_SEPARATOR_S "%s", _("feedlist.opml"));
+	}
+	if(!import_OPML_feedlist(filename11, node, node->source, FALSE, TRUE))
+		g_error("Fatal: Feed list import failed!");
+	g_free(filename11);
+	feedlistImport = FALSE;
+
+#ifdef USE_DBUS
+	if(!getBooleanConfValue(DISABLE_DBUS)) {
+		/* Start listening on the dbus for new subscriptions */	
+		debug0(DEBUG_GUI, "Registering with DBUS...");
+		default_source_dbus_connect();
+	} else {
+		g_print("DBUS disabled by user request...");
+	}
+#else
+	debug0(DEBUG_GUI, "Compiled without DBUS support.");
+#endif
+
+	debug_exit("default_source_source_import");
+}
+
+static void default_source_source_export(nodePtr node) {
+	gchar	*filename;
+	
+	if(feedlistImport)
+		return;
+
+	debug_enter("default_source_source_export");
+	
+	g_assert(node->source->root == feedlist_get_root());
+
+	filename = default_source_source_get_feedlist(node);
+	export_OPML_feedlist(filename, node->source->root, TRUE);
+	g_free(filename);
+
+	debug_exit("default_source_source_export");
+}
+
+static void default_source_source_auto_update(nodePtr node) {
+
+	node_foreach_child(node, node_request_auto_update);
+}
 
 /* root node type definition */
 
