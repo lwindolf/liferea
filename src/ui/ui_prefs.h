df67f47337ad881064ddcda520a7de2588544f9b
Lars Lindner <lars.lindner@gmail.com>
2006-12-11 01:50:09 +0000

    fixing browser launching problems with other browsers than Firefox that do not accept "," escaping

--- src/ui/ui_prefs.h
+++ src/ui/ui_prefs.h
@@ -1,8 +1,8 @@
 /**
  * @file ui_prefs.h program preferences
  *
- * Copyright (C) 2004-2005 Nathan J. Conrad <t98502@users.sourceforge.net>
- * Copyright (C) 2004-2005 Lars Lindner <lars.lindner@gmx.net>
+ * Copyright (C) 2004-2006 Nathan J. Conrad <t98502@users.sourceforge.net>
+ * Copyright (C) 2004-2006 Lars Lindner <lars.lindner@gmx.net>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -24,9 +24,56 @@
 
 #include <gtk/gtk.h>
 
-gchar * prefs_get_browser_remotecmd();
-gchar * prefs_get_browser_cmd();
-gchar * prefs_get_download_cmd();
+/** Browser preference definition structure */
+struct browser {
+	gchar *id;			/**< Unique ID used in storing the prefs */
+	gchar *display;			/**< Name to display in the prefs */
+	gchar *defaultplace;		/**< Default command.... Use %s to specify URL. This command is called in the background. */
+	gchar *existingwin;		/**< Optional command variant for opening in existing window */
+	gchar *existingwinremote;	/**< Optional command variant for opening in existing window with remote protocol */
+	gchar *newwin;			/**< Optional command variant for opening in new window */
+	gchar *newwinremote;		/**< Optional command variant for opening in new window with remote protocol */
+	gchar *newtab;			/**< Optional command variant for opening in new tab */
+	gchar *newtabremote;		/**< Optional command variant for opening in new tab with remote protocol */
+	gboolean escapeRemote;		/**< Flag to indicate wether "," escaping for remote commands is necessary */
+};
+
+/** 
+ * Returns the browser definition structure for the currently
+ * configured external browser or NULL if a user defined 
+ * browser command is defined.
+ *
+ * @returns browser definition
+ */
+struct browser * prefs_get_browser(void);
+
+/**
+ * Returns a shell command format string which can be used to create
+ * a browser launch command. The string will contain exactly one %s
+ * to fill in the URL. 
+ *
+ * @param browser	browser definition (or NULL) as returned
+ *			by prefs_get_browser()
+ * @param remote	TRUE if remote command variant is requested
+ * @param fallback	TRUE if the default command is to be returned
+ *			if the specific launch type is not available.
+ *			If set to FALSE no command might be returned.
+ *
+ * @returns a newly allocated command string
+ */
+gchar * prefs_get_browser_command(struct browser *browser, gboolean remote, gboolean fallback);
+
+/**
+ * Returns a shell command line with exactly two %s format codes
+ * that can be used for launching an enclosure download program.
+ * The first %s is to be replaced with the output filename, the
+ * second %s is to be replaced with the URL to download.
+ *
+ * @return the command string
+ */
+const gchar * prefs_get_download_cmd(void);
+
+/* GUI callbacks */
 
 void 
 on_prefbtn_clicked                     (GtkButton       *button,
