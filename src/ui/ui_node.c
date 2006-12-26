911ce34e4f8b160e6b8e4745acba4ded29c55e61
Lars Lindner <lars.lindner@gmail.com>
2006-12-26 23:16:47 +0000

    ...

--- src/ui/ui_node.c
+++ src/ui/ui_node.c
@@ -56,7 +56,8 @@ gboolean ui_node_is_folder_expanded(nodePtr folder) {
 	GtkTreeIter	*iter;
 	gboolean 	expanded = FALSE;
 
-	if( (iter = ui_node_to_iter(folder)) ) {
+	iter = ui_node_to_iter(folder);
+	if(iter) {
 		path = gtk_tree_model_get_path(GTK_TREE_MODEL(feedstore), iter);
 		expanded = gtk_tree_view_row_expanded(GTK_TREE_VIEW(lookup_widget(mainwindow, "feedlist")), path);
 		gtk_tree_path_free(path);
