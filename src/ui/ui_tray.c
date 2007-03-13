56df84331e4f47e0b1a674d577ced900bbf35810
Lars Lindner <lars.lindner@gmail.com>
2007-03-13 23:03:55 +0000

    back merging tray icon number display from 1.3

--- src/ui/ui_tray.c
+++ src/ui/ui_tray.c
@@ -1,7 +1,7 @@
 /**
  * @file ui_tray.c tray icon handling
  * 
- * Copyright (C) 2003-2005 Lars Lindner <lars.lindner@gmx.net>
+ * Copyright (C) 2003-2007 Lars Lindner <lars.lindner@gmail.com>
  * Copyright (C) 2004 Christophe Barbe <christophe.barbe@ufies.org>
  *
  * This program is free software; you can redistribute it and/or modify
@@ -23,10 +23,11 @@
 #  include <config.h>
 #endif
 
+#include <math.h>
+#include <string.h>
 #include <gtk/gtk.h>
 #include <gdk/gdk.h>
 
-#include "conf.h"
 #include "callbacks.h"
 #include "eggtrayicon.h"
 #include "support.h"
@@ -36,28 +37,36 @@
 #include "ui_mainwindow.h"
 #include "update.h"
 
-#define NO_NEW_ITEMS	0
-#define NEW_ITEMS	1
+// FIXME: determine this from Pango or Cairo somehow...
+#define	FONT_CHAR_WIDTH	6
+#define FONT_CHAR_HEIGHT 8
+
+#define TRAY_ICON_WIDTH	16
+#define TRAY_ICON_HEIGHT 16
 
 extern GdkPixbuf	*icons[];
 
 extern GtkWidget	*mainwindow;
 
-static GtkWidget	*eventbox = NULL;
-static EggTrayIcon 	*tray_icon =  NULL;
-static GtkTooltips	*tray_icon_tips = NULL;
-static GtkWidget	*image = NULL;		/* the image in the notification area */
-static int trayCount = 0;
-static GdkPixbuf *currentIcon = NULL;
+static struct trayIcon_priv {
+	int		trayCount;		/**< reference counter */
+	GdkPixbuf	*currentIcon;		/**< currently displayed icon */
+	
+	EggTrayIcon 	*widget;		/**< the tray icon widget */
+	GtkWidget	*image;			/**< render widget in the notification area */
+	GtkWidget	*alignment;
+	GtkWidget	*eventBox;
+	GtkTooltips	*toolTips;
+} *trayIcon_priv = NULL;
 
-static void installTrayIcon(void);
+static void ui_tray_install(void);
 
 void ui_tray_tooltip_set(gchar *message) {
 	GtkTooltipsData	*data = NULL;
 	
-	g_assert(tray_icon_tips);
+	g_assert(trayIcon_priv->toolTips);
 
-	data = gtk_tooltips_data_get(GTK_WIDGET(tray_icon));
+	data = gtk_tooltips_data_get(GTK_WIDGET(trayIcon_priv->widget));
 
 	if(NULL != data) {
 		/* FIXME: Is this necessary? Gtk 2.2.4 frees these strings
@@ -68,45 +77,108 @@ void ui_tray_tooltip_set(gchar *message) {
 		data->tip_private = NULL;
 	}
 	
-	gtk_tooltips_set_tip(GTK_TOOLTIPS(tray_icon_tips), 
-					 GTK_WIDGET(eventbox),
-					 message, message);
+	gtk_tooltips_set_tip(GTK_TOOLTIPS(trayIcon_priv->toolTips), 
+	                     GTK_WIDGET(trayIcon_priv->eventBox),
+			     message, message);
 }
 
-static void ui_tray_icon_set(GdkPixbuf *icon) {
-
-	g_assert(tray_icon);
+static void ui_tray_expose_cb() {
+	cairo_t		*c;
+	gchar		*str;
+	guint		newItems;
+	GtkRequisition	requisition;
 	
-	/* Skip loading icon if already displayed. */
-	if (icon == currentIcon)
-		return;
-	currentIcon = icon;
+	gtk_widget_size_request(GTK_WIDGET(trayIcon_priv->widget), &requisition);
 	
-	if(NULL != image)
-		gtk_widget_destroy(image);
+	/* We expect currentIcon to be a 16x16 image and we will 
+	   render a colored area with height 10 in the middle of 
+	   the image with 8pt text inside */
 
-	image = gtk_image_new_from_pixbuf(icon);
-	gtk_container_add(GTK_CONTAINER(eventbox), image);
-	gtk_widget_show_all(GTK_WIDGET(tray_icon));
+	gdk_draw_pixbuf(GDK_DRAWABLE(trayIcon_priv->image->window), 
+	                NULL, trayIcon_priv->currentIcon, 0, 0, 
+			(requisition.width > TRAY_ICON_WIDTH)?(requisition.width - TRAY_ICON_WIDTH)/2 - 1:0, 
+			(requisition.height > TRAY_ICON_HEIGHT)?(requisition.height - TRAY_ICON_HEIGHT)/2 - 1:0, 
+	                gdk_pixbuf_get_height(trayIcon_priv->currentIcon),
+			gdk_pixbuf_get_width(trayIcon_priv->currentIcon), 
+			GDK_RGB_DITHER_NONE, 0, 0);
+			
+	newItems = feedlist_get_new_item_count();
+	if(newItems > 0) {
+		guint textWidth, textStart;
+		str = g_strdup_printf("%d", newItems);
+		textWidth = strlen(str) * FONT_CHAR_WIDTH;
+		
+		if(textWidth + 2 > TRAY_ICON_WIDTH)
+			textStart = 1;
+		else
+			textStart = TRAY_ICON_WIDTH/2 - textWidth/2;
+
+		c = gdk_cairo_create(trayIcon_priv->image->window);
+		cairo_rectangle(c, textStart - 1, 3, textWidth + 1, FONT_CHAR_HEIGHT + 2);
+		cairo_set_source_rgb(c, 1, 0.50, 0.10);	// orange
+		cairo_fill(c);
+
+		cairo_set_source_rgb(c, 1, 1, 1);		
+		cairo_move_to(c, textStart - 1, 3 + FONT_CHAR_HEIGHT);
+		cairo_select_font_face(c, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
+		cairo_set_font_size(c, FONT_CHAR_HEIGHT + 2);
+		cairo_show_text(c, str);
+		cairo_destroy(c);
+
+		g_free(str);
+	}
+}
+
+static void ui_tray_icon_set(gint newItems, GdkPixbuf *icon) {
+	guint 	width;
+	
+	width = ((guint)log10(newItems) + 1) * FONT_CHAR_WIDTH;
+	width += 2; /* number color border */
+	width += 2; /* tray icon padding */;
+	if(width < 16)
+		width = 16;
+
+	g_assert(trayIcon_priv->widget);
+	
+	trayIcon_priv->currentIcon = icon;
+	
+	if(trayIcon_priv->alignment)
+		gtk_widget_destroy(trayIcon_priv->alignment);
+	
+	trayIcon_priv->alignment = gtk_alignment_new(0.5, 0.5, 0.0, 0.0);
+	trayIcon_priv->image = gtk_drawing_area_new();
+	gtk_widget_set_size_request(trayIcon_priv->image, width, 16);
+	g_signal_connect(G_OBJECT(trayIcon_priv->image), "expose_event",  
+                         G_CALLBACK(ui_tray_expose_cb), NULL);
+ 
+	gtk_container_add(GTK_CONTAINER(trayIcon_priv->eventBox), trayIcon_priv->alignment);
+	gtk_container_add(GTK_CONTAINER(trayIcon_priv->alignment), trayIcon_priv->image);
+	gtk_widget_show_all(GTK_WIDGET(trayIcon_priv->widget));
 }
 
 void ui_tray_update(void) {
 	gint	newItems, unreadItems;
 	gchar	*msg, *tmp;
 	
-	if(!tray_icon)
+	if(!trayIcon_priv)
 		return;
 
 	newItems = feedlist_get_new_item_count();
 	unreadItems = feedlist_get_unread_item_count();
 		
 	if(newItems != 0) {
-		if(update_is_online()) ui_tray_icon_set(icons[ICON_AVAILABLE]);
-		else ui_tray_icon_set(icons[ICON_AVAILABLE_OFFLINE]);
+		if(update_is_online())
+			ui_tray_icon_set(newItems, icons[ICON_AVAILABLE]);
+		else
+			ui_tray_icon_set(newItems, icons[ICON_AVAILABLE_OFFLINE]);
+			
 		msg = g_strdup_printf(ngettext("%d new item", "%d new items", newItems), newItems);
 	} else {
-		if(update_is_online()) ui_tray_icon_set(icons[ICON_EMPTY]);
-		else ui_tray_icon_set(icons[ICON_EMPTY_OFFLINE]);
+		if(update_is_online())
+			ui_tray_icon_set(newItems, icons[ICON_EMPTY]);
+		else
+			ui_tray_icon_set(newItems, icons[ICON_EMPTY_OFFLINE]);
+			
 		msg = g_strdup(_("No new items"));
 	}
 
@@ -126,12 +198,12 @@ void ui_tray_update(void) {
 static void tray_icon_pressed(GtkWidget *button, GdkEventButton *event, EggTrayIcon *icon) {
 	
 	switch(event->button) {
-	case 1:
-		ui_mainwindow_toggle_visibility(NULL, NULL);
-		break;
-	case 3:
-		gtk_menu_popup(ui_popup_make_systray_menu(), NULL, NULL, NULL, NULL, event->button, event->time);
-		break;
+		case 1:
+			ui_mainwindow_toggle_visibility(NULL, NULL);
+			break;
+		case 3:
+			gtk_menu_popup(ui_popup_make_systray_menu(), NULL, NULL, NULL, NULL, event->button, event->time);
+			break;
 	}
 						  
 	return;
@@ -139,7 +211,7 @@ static void tray_icon_pressed(GtkWidget *button, GdkEventButton *event, EggTrayI
 
 static gboolean ui_tray_create_cb() {
 
-	installTrayIcon();
+	ui_tray_install();
 	
 	return FALSE; /* for when we're called by the glib idle handler */
 }
@@ -153,12 +225,10 @@ static void ui_tray_embedded_cb(GtkWidget *widget, void *data) {
 
 static void ui_tray_destroyed_cb(GtkWidget *widget, void *data) {
 
-	g_object_unref(G_OBJECT(tray_icon));
-
-	image = NULL;
-	tray_icon = NULL;
-	currentIcon = NULL;
-	trayCount--;
+	g_object_unref(G_OBJECT(trayIcon_priv->widget));
+	g_free(trayIcon_priv);
+	trayIcon_priv = NULL;
+	
 	ui_mainwindow_tray_remove();
 	
 	/* And make it re-appear when the notification area reappears */
@@ -166,65 +236,76 @@ static void ui_tray_destroyed_cb(GtkWidget *widget, void *data) {
 	
 }
 
-static void installTrayIcon(void) {
+static void ui_tray_install(void) {
 
-	g_assert(NULL == tray_icon);
+	g_assert(!trayIcon_priv);
+	trayIcon_priv = g_new0(struct trayIcon_priv, 1);
 
-	tray_icon = egg_tray_icon_new(PACKAGE);
-	eventbox = gtk_event_box_new();
+	trayIcon_priv->widget = egg_tray_icon_new(PACKAGE);
+	trayIcon_priv->eventBox = gtk_event_box_new();
 	
-	g_signal_connect(eventbox, "button_press_event", G_CALLBACK(tray_icon_pressed), tray_icon);
-	g_signal_connect(G_OBJECT(tray_icon), "embedded", G_CALLBACK(ui_tray_embedded_cb), NULL);
-	g_signal_connect(G_OBJECT(tray_icon), "destroy", G_CALLBACK(ui_tray_destroyed_cb), NULL);
+//		GtkAlignment	*align;
 	
-	ui_dnd_setup_URL_receiver(eventbox);
+	g_signal_connect(trayIcon_priv->eventBox, "button_press_event",
+	                 G_CALLBACK(tray_icon_pressed), trayIcon_priv->widget);
+	g_signal_connect(G_OBJECT(trayIcon_priv->widget), "embedded",
+	                 G_CALLBACK(ui_tray_embedded_cb), NULL);
+	g_signal_connect(G_OBJECT(trayIcon_priv->widget), "destroy",
+	                 G_CALLBACK(ui_tray_destroyed_cb), NULL);
 	
-	gtk_container_add(GTK_CONTAINER(tray_icon), eventbox);
-	g_object_ref(G_OBJECT(tray_icon));
+	ui_dnd_setup_URL_receiver(trayIcon_priv->eventBox);
 	
-	tray_icon_tips = gtk_tooltips_new();
+	gtk_container_add(GTK_CONTAINER(trayIcon_priv->widget), trayIcon_priv->eventBox);
+	g_object_ref(G_OBJECT(trayIcon_priv->widget));
+	
+	trayIcon_priv->toolTips = gtk_tooltips_new();
 	ui_tray_update();
-	trayCount++;
+	trayIcon_priv->trayCount++;
 }
 
-static void removeTrayIcon() {
+static void ui_tray_remove(void) {
 
-	g_assert(tray_icon != NULL);
+	g_assert(trayIcon_priv->widget);
 	
-	g_signal_handlers_disconnect_by_func(G_OBJECT(tray_icon), G_CALLBACK(ui_tray_destroyed_cb), NULL);
-	gtk_widget_destroy(image);
-	image = NULL;
-	g_object_unref(G_OBJECT(tray_icon));
-	gtk_object_destroy(GTK_OBJECT (tray_icon));
-	tray_icon = NULL;
-	currentIcon = NULL;
-	trayCount--;
+	g_signal_handlers_disconnect_by_func(G_OBJECT(trayIcon_priv->widget),
+	                                     G_CALLBACK(ui_tray_destroyed_cb), NULL);
+	gtk_widget_destroy(trayIcon_priv->image);
+	g_object_unref(G_OBJECT(trayIcon_priv->widget));
+	gtk_object_destroy(GTK_OBJECT(trayIcon_priv->widget));
+	g_free(trayIcon_priv);
+	trayIcon_priv = NULL;
+
 	ui_mainwindow_tray_remove();
 }
 
 void ui_tray_enable(gboolean enabled) {
 
 	if(enabled) {
-		if(tray_icon == NULL)
-			installTrayIcon();
+		if(!trayIcon_priv)
+			ui_tray_install();
 	} else {
-		if(tray_icon != NULL)
-			removeTrayIcon();
+		if(trayIcon_priv)
+			ui_tray_remove();
 	}
 }
 
 int ui_tray_get_count() {
-	return trayCount;
+	return trayIcon_priv?trayIcon_priv->trayCount:0;
 }
 
 gboolean ui_tray_get_origin(gint *x, gint *y) {
-	if ( tray_icon == NULL ) {
+
+	if(!trayIcon_priv)
 		return FALSE;
-	}
-	gdk_window_get_origin (GTK_WIDGET (tray_icon)->window, x, y);	
+
+	gdk_window_get_origin(GTK_WIDGET(trayIcon_priv->widget)->window, x, y);	
 	return TRUE;
 }
 
-void ui_tray_size_request (GtkRequisition *requisition) {
-	gtk_widget_size_request ( GTK_WIDGET (tray_icon), requisition);
+void ui_tray_size_request(GtkRequisition *requisition) {
+
+	if(!trayIcon_priv)
+		return;
+
+	gtk_widget_size_request(GTK_WIDGET(trayIcon_priv->widget), requisition);
 }
