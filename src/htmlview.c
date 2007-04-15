0f856ca3f8f0f116bd5ab67a9cd181a184f3a8bf
Lars Lindner <lars.lindner@gmail.com>
2007-04-15 22:43:52 +0000

    rewrite of the html view to fix ordering in 2 pane mode

--- src/htmlview.c
+++ src/htmlview.c
@@ -36,140 +36,220 @@
 
 extern htmlviewPluginPtr htmlviewPlugin;
 
-static struct htmlView_priv {
-	GHashTable	*htmlChunks;	/**< cache of HTML chunks of all displayed items */
+static struct htmlView_priv 
+{
+	GHashTable	*chunkHash;	/**< cache of HTML chunks of all displayed items */
+	GSList		*orderedChunks;	/**< ordered list of chunks */
 	nodePtr		node;		/**< the node whose items are displayed */
 	guint		missingContent;	/**< counter for items without content */
 } htmlView_priv;
 
-void htmlview_init(void) {
+typedef struct htmlChunk 
+{
+	gulong 		id;	/**< item id */
+	gchar		*html;	/**< the rendered HTML (or NULL if not yet rendered) */
+	time_t		date;	/**< date as sorting criteria */
+} *htmlChunkPtr;
 
-	htmlView_priv.htmlChunks = NULL;
-	htmlview_clear();
+static void
+htmlview_chunk_free (htmlChunkPtr chunk) 
+{
+	g_free (chunk->html);
+	g_free (chunk);
 }
 
-void htmlview_clear(void) {
+static gint
+htmlview_chunk_sort (gconstpointer a,
+                     gconstpointer b) 
+{
+	return (((htmlChunkPtr)a)->date) - (((htmlChunkPtr)b)->date);
+}
 
-	if(htmlView_priv.htmlChunks)
-		g_hash_table_destroy(htmlView_priv.htmlChunks);
+void 
+htmlview_init (void) 
+{
+	htmlView_priv.chunkHash = NULL;
+	htmlView_priv.orderedChunks = NULL;
+	htmlview_clear ();
+}
 
-	htmlView_priv.htmlChunks = g_hash_table_new_full(g_direct_hash, g_direct_equal, NULL, g_free);
+void
+htmlview_clear (void) 
+{
+	if (htmlView_priv.chunkHash)
+		g_hash_table_destroy (htmlView_priv.chunkHash);
+
+	GSList	*iter = htmlView_priv.orderedChunks;
+	while (iter)
+	{
+		htmlview_chunk_free (iter->data);
+		iter = g_slist_next (iter);
+	}
+
+	if (htmlView_priv.orderedChunks)
+		g_slist_free (htmlView_priv.orderedChunks);
+	
+	htmlView_priv.chunkHash = g_hash_table_new (g_direct_hash, g_direct_equal);
+	htmlView_priv.orderedChunks = NULL;
 	htmlView_priv.missingContent = 0;
 }
 
-void htmlview_set_displayed_node(nodePtr node) {
-
-	g_assert(0 == g_hash_table_size(htmlView_priv.htmlChunks));
+void
+htmlview_set_displayed_node (nodePtr node) 
+{
+	g_assert (0 == g_hash_table_size (htmlView_priv.chunkHash));
 	htmlView_priv.node = node;
 }
 
-void htmlview_add_item(itemPtr item) {
+void
+htmlview_add_item (itemPtr item) 
+{
+	htmlChunkPtr	chunk;
+	
+	debug1 (DEBUG_HTML, "HTML view: adding \"%s\"", item_get_title (item));
 
-	debug1(DEBUG_HTML, "HTML view: adding \"%s\"", item_get_title(item));
-	g_hash_table_insert(htmlView_priv.htmlChunks, item, NULL);
+	chunk = g_new0 (struct htmlChunk, 1);
+	chunk->id = item->id;
+	g_hash_table_insert (htmlView_priv.chunkHash, GUINT_TO_POINTER (item->id), chunk);
+	
+	htmlView_priv.orderedChunks = g_slist_insert_sorted (htmlView_priv.orderedChunks, chunk, htmlview_chunk_sort);
 		
-	if(!item_get_description(item) || (0 == strlen(item_get_description(item))))
+	if (!item_get_description (item) || (0 == strlen (item_get_description (item))))
 		htmlView_priv.missingContent++;	
 }
 
-void htmlview_remove_item(itemPtr item) {
+void
+htmlview_remove_item (itemPtr item) 
+{
+	htmlChunkPtr	chunk;
 
-	debug1(DEBUG_HTML, "HTML view: removing \"%s\"", item_get_title(item));
-	g_hash_table_remove(htmlView_priv.htmlChunks, item);
+	debug1 (DEBUG_HTML, "HTML view: removing \"%s\"", item_get_title (item));
+	
+	chunk = g_hash_table_lookup (htmlView_priv.chunkHash, GUINT_TO_POINTER (item->id));
+	if (chunk) 
+	{
+		g_hash_table_remove (htmlView_priv.chunkHash, GUINT_TO_POINTER (item->id));
+		htmlView_priv.orderedChunks = g_slist_remove (htmlView_priv.orderedChunks, chunk);
+		htmlview_chunk_free (chunk);
+	}
 }
 
-void htmlview_select_item(itemPtr item) {
-
+void
+htmlview_select_item (itemPtr item) 
+{
 	debug1(DEBUG_HTML, "HTML view: selecting \"%s\"", item_get_title(item));	
 	/* nothing to do... */
 }
 
-void htmlview_update_item(itemPtr item) {
-
+void
+htmlview_update_item (itemPtr item) 
+{
+	htmlChunkPtr	chunk;
+	
 	/* ensure rerendering on next update by replace old HTML chunk with NULL */
-	g_hash_table_insert(htmlView_priv.htmlChunks, item, NULL);
+	chunk = (htmlChunkPtr) g_hash_table_lookup (htmlView_priv.chunkHash, GUINT_TO_POINTER (item->id));
+	if (chunk) 
+	{
+		g_free (chunk->html);
+		chunk->html = NULL;
+	}
 }
 
-static xmlDocPtr itemset_to_xml(nodePtr node) {
+// FIXME: bad naming -> unclear concept!
+static xmlDocPtr
+itemset_to_xml (nodePtr node) 
+{
 	xmlDocPtr 	doc;
 	xmlNodePtr 	itemSetNode;
 	
-	doc = xmlNewDoc("1.0");
-	itemSetNode = xmlNewDocNode(doc, NULL, "itemset", NULL);
+	doc = xmlNewDoc ("1.0");
+	itemSetNode = xmlNewDocNode (doc, NULL, "itemset", NULL);
 	
-	xmlDocSetRootElement(doc, itemSetNode);
+	xmlDocSetRootElement (doc, itemSetNode);
 	
-	xmlNewTextChild(itemSetNode, NULL, "favicon", node_get_favicon_file(node));
-	xmlNewTextChild(itemSetNode, NULL, "title", node_get_title(node));
+	xmlNewTextChild (itemSetNode, NULL, "favicon", node_get_favicon_file (node));
+	xmlNewTextChild (itemSetNode, NULL, "title", node_get_title (node));
 
-	if(NODE_TYPE_FEED == node->type) {
-	       xmlNewTextChild(itemSetNode, NULL, "source", subscription_get_source(node->subscription));
-	       xmlNewTextChild(itemSetNode, NULL, "link", feed_get_html_url(node->data));
-	}
+	if (subscription_get_source (node->subscription))
+	       xmlNewTextChild (itemSetNode, NULL, "source", subscription_get_source (node->subscription));
+
+	// FIXME: should not be node type specific!	       
+	if (NODE_TYPE_FEED == node->type)
+	       xmlNewTextChild (itemSetNode, NULL, "link", feed_get_html_url (node->data));
 
 	return doc;
 }
 
-static gchar * htmlview_render_item(itemPtr item, gboolean summaryMode) {
+static gchar *
+htmlview_render_item (itemPtr item, 
+                      gboolean summaryMode) 
+{
 	renderParamPtr	params;
 	gchar		*output = NULL, *baseUrl;
 	nodePtr		node;
 	xmlDocPtr	doc;
 
-	debug_enter("htmlview_render_item");
+	debug_enter ("htmlview_render_item");
 
-	baseUrl = common_uri_escape(node_get_base_url(htmlView_priv.node));
+	baseUrl = common_uri_escape (node_get_base_url (htmlView_priv.node));
 
 	/* do the XML serialization */
-	doc = itemset_to_xml(htmlView_priv.node);
+	doc = itemset_to_xml (htmlView_priv.node);
 			
-	item_to_xml(item, xmlDocGetRootElement(doc));
+	item_to_xml(item, xmlDocGetRootElement (doc));
 			
-	node = node_from_id(item->nodeId);
-	if(NODE_TYPE_FEED == node->type) {
+	node = node_from_id (item->nodeId);
+	if (NODE_TYPE_FEED == node->type) 
+	{
 		xmlNodePtr feed;
 		feed = xmlNewChild(xmlDocGetRootElement(doc), NULL, "feed", NULL);
 		feed_to_xml(node, feed);
 	}
 	
 	/* do the XSLT rendering */
-	params = render_parameter_new();
-	render_parameter_add(params, "pixmapsDir='file://" PACKAGE_DATA_DIR G_DIR_SEPARATOR_S PACKAGE G_DIR_SEPARATOR_S "pixmaps" G_DIR_SEPARATOR_S "'");
-	render_parameter_add(params, "baseUrl='%s'", baseUrl);
-	render_parameter_add(params, "summary='%d'", summaryMode?1:0);
-	output = render_xml(doc, "item", params);
+	params = render_parameter_new ();
+	render_parameter_add (params, "pixmapsDir='file://" PACKAGE_DATA_DIR G_DIR_SEPARATOR_S PACKAGE G_DIR_SEPARATOR_S "pixmaps" G_DIR_SEPARATOR_S "'");
+	render_parameter_add (params, "baseUrl='%s'", baseUrl);
+	render_parameter_add (params, "summary='%d'", summaryMode?1:0);
+	output = render_xml (doc, "item", params);
 	
 	/* For debugging use: xmlSaveFormatFile("/tmp/test.xml", doc, 1); */
-	xmlFreeDoc(doc);
-	g_free(baseUrl);
+	xmlFreeDoc (doc);
+	g_free (baseUrl);
 	
-	debug_exit("htmlview_render_item");
+	debug_exit ("htmlview_render_item");
 
 	return output;
 }
 
-void htmlview_start_output(GString *buffer, const gchar *base, gboolean css, gboolean script) { 
+void 
+htmlview_start_output (GString *buffer,
+                       const gchar *base,
+		       gboolean css,
+		       gboolean script) 
+{	
+	g_string_append (buffer, "<?xml version=\"1.0\" encoding=\"utf-8\"?><!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n");
+	g_string_append (buffer, "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n");
+	g_string_append (buffer, "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n");
+	g_string_append (buffer, "<head>\n<title></title>");
+	g_string_append (buffer, "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />");
 	
-	g_string_append(buffer, "<?xml version=\"1.0\" encoding=\"utf-8\"?><!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\"\n");
-	g_string_append(buffer, "\"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n");
-	g_string_append(buffer, "<html xmlns=\"http://www.w3.org/1999/xhtml\">\n");
-	g_string_append(buffer, "<head>\n<title></title>");
-	g_string_append(buffer, "<meta http-equiv=\"Content-Type\" content=\"text/html; charset=utf-8\" />");
-	
-	if(base) {
-		gchar *escBase = g_markup_escape_text(base, -1);
-		g_string_append(buffer, "<base href=\"");
-		g_string_append(buffer, escBase);
-		g_string_append(buffer, "\" />\n");
-		g_free(escBase);
+	if (base) 
+	{
+		gchar *escBase = g_markup_escape_text (base, -1);
+		g_string_append (buffer, "<base href=\"");
+		g_string_append (buffer, escBase);
+		g_string_append (buffer, "\" />\n");
+		g_free (escBase);
 	}
 
-	if(css)
-		g_string_append(buffer, render_get_css(htmlviewPlugin->externalCss));
+	if (css)
+		g_string_append (buffer, render_get_css (htmlviewPlugin->externalCss));
 	
 	/* add predefined scripts to be used for item menu */
-	if(script) {
-		g_string_append(buffer,  "<script language=\"javascript\" type=\"text/javascript\">"
+	if (script) 
+	{
+		g_string_append (buffer,  "<script language=\"javascript\" type=\"text/javascript\">"
 		"var popupTimeout;"
 		"var lastId;"
 		""
@@ -205,46 +285,54 @@ void htmlview_start_output(GString *buffer, const gchar *base, gboolean css, gbo
 		"</script>");
 	}
 	
-	g_string_append(buffer, "</head>\n<body>");
+	g_string_append (buffer, "</head>\n<body>");
 }
 
-void htmlview_finish_output(GString *buffer) {
-
-	g_string_append(buffer, "</body></html>"); 
+void
+htmlview_finish_output (GString *buffer) 
+{
+	g_string_append (buffer, "</body></html>"); 
 }
 
-void htmlview_update(GtkWidget *widget, guint mode) {
-	GList		*iter = NULL;
+void
+htmlview_update (GtkWidget *widget,
+                 guint mode) 
+{
+	GSList		*iter;
 	GString		*output;
-	itemSetPtr	itemSet;
 	itemPtr		item = NULL;
-	gchar		*chunk,	*baseURL;
+	gchar		*baseURL;
 	gboolean	summaryMode;
 		
-	if(!htmlView_priv.node) {
-		debug0(DEBUG_HTML, "clearing HTML view as nothing is selected");
-		ui_htmlview_clear(widget);
+	if (!htmlView_priv.node)
+	{
+		debug0 (DEBUG_HTML, "clearing HTML view as nothing is selected");
+		ui_htmlview_clear (widget);
 		return;
 	}
 	
-	baseURL = (gchar *)node_get_base_url(htmlView_priv.node);
-	if(baseURL)
-		baseURL = g_markup_escape_text(baseURL, -1);
+	baseURL = (gchar *) node_get_base_url (htmlView_priv.node);
+	if (baseURL)
+		baseURL = g_markup_escape_text (baseURL, -1);
 		
-	output = g_string_new(NULL);
-	htmlview_start_output(output, baseURL, TRUE, TRUE);
+	output = g_string_new (NULL);
+	htmlview_start_output (output, baseURL, TRUE, TRUE);
 
 	/* HTML view updating means checking which items
 	   need to be updated, render them and then 
 	   concatenate everything from cache and output it */
-	switch(mode) {
+	switch (mode) 
+	{
 		case ITEMVIEW_SINGLE_ITEM:
-			item = itemlist_get_selected();
-			if(item) {
-				chunk = htmlview_render_item(item, FALSE);
-				if(chunk)
-					g_string_append(output, chunk);
-				g_free(chunk);
+			item = itemlist_get_selected ();
+			if (item) 
+			{
+				gchar *html = htmlview_render_item (item, FALSE);
+				if (html)
+				{
+					g_string_append (output, html);
+					g_free (html);
+				}
 			}
 			break;
 		case ITEMVIEW_ALL_ITEMS:
@@ -259,41 +347,50 @@ void htmlview_update(GtkWidget *widget, guint mode) {
 	        		      (htmlView_priv.missingContent > 3);
 
 			/* concatenate all items */
-			itemSet = node_get_itemset(htmlView_priv.node);
-			iter = itemSet->ids;
-			while(iter) {
+			iter = htmlView_priv.orderedChunks;
+			while (iter) 
+			{
 				/* try to retrieve item HTML chunk from cache */
-				chunk = g_hash_table_lookup(htmlView_priv.htmlChunks, iter->data);
-					
+				htmlChunkPtr chunk = (htmlChunkPtr)iter->data;
+				
 				/* if not found: render new item now and add to cache */
-				if(!chunk) {
-					item = item_load(GPOINTER_TO_UINT(iter->data));
-					debug1(DEBUG_HTML, "rendering item to HTML view: >>>%s<<<", item_get_title(item));
-					chunk = htmlview_render_item(item, summaryMode);
-					item_unload(item);
-					g_hash_table_insert(htmlView_priv.htmlChunks, iter->data, chunk);					
+				if (!chunk->html) 
+				{
+					item = item_load (chunk->id);
+					if (item)
+					{
+						debug1 (DEBUG_HTML, "rendering item to HTML view: >>>%s<<<", item_get_title (item));
+						chunk->html = htmlview_render_item (item, summaryMode);
+						item_unload (item);
+					}
 				}
+				
+				if (chunk->html)
+					g_string_append (output, chunk->html);
 					
-				g_string_append(output, chunk);
-				iter = g_list_next(iter);
+				iter = g_slist_next (iter);
 			}
-			itemset_free(itemSet);
 			break;
 		case ITEMVIEW_NODE_INFO:
-			chunk = node_render(htmlView_priv.node);
-			g_string_append(output, chunk);
-			g_free(chunk);
+			{
+				gchar *html = node_render (htmlView_priv.node);
+				if (html)
+				{
+					g_string_append (output, html);
+					g_free (html);
+				}
+			}
 			break;
 		default:
-			g_warning("HTML view: invalid viewing mode!!!");
+			g_warning ("HTML view: invalid viewing mode!!!");
 			break;
 	}
 	
-	htmlview_finish_output(output);
+	htmlview_finish_output (output);
 
-	debug1(DEBUG_HTML, "writing %d bytes to HTML view", strlen(output->str));
-	ui_htmlview_write(widget, output->str, baseURL);
+	debug1 (DEBUG_HTML, "writing %d bytes to HTML view", strlen (output->str));
+	ui_htmlview_write (widget, output->str, baseURL);
 	
-	g_string_free(output, TRUE);
-	g_free(baseURL);
+	g_string_free (output, TRUE);
+	g_free (baseURL);
 }
