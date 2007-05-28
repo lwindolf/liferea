ec3a826ffc9de80ef3ef7d51fe9abe850b86af1c
Lars Lindner <lars.lindner@gmail.com>
2007-05-28 22:37:25 +0000

    sync to 1.2.x, makes auto-launch-links work again

--- src/mozilla/mozembed.c
+++ src/mozilla/mozembed.c
@@ -226,13 +226,15 @@ mozembed_dom_mouse_click_cb (GtkMozEmbed *dummy, gpointer dom_event, gpointer em
  * to keep the new document from being loaded. The uri argument is the
  * uri that's going to be loaded.
  */
-static gint mozembed_open_uri_cb (GtkMozEmbed *embed, const char *uri, gpointer data) {
-	
-	if((FALSE == ui_htmlview_is_special_url(uri)) && 
-	   (GPOINTER_TO_INT(g_object_get_data(G_OBJECT(embed), "internal_browsing")))) {
+static gint 
+mozembed_open_uri_cb (GtkMozEmbed *embed, const char *uri, gpointer data) 
+{	
+	if ((FALSE == ui_htmlview_is_special_url (uri)) && 
+ 	    (GPOINTER_TO_INT (g_object_get_data (G_OBJECT (embed), "internal_browsing")) ||
+	    getBooleanConfValue (BROWSE_INSIDE_APPLICATION))) {
 		return FALSE;
 	} else {
-		ui_htmlview_launch_URL(GTK_WIDGET(data), (gchar *)uri, UI_HTMLVIEW_LAUNCH_DEFAULT);
+		ui_htmlview_launch_URL (GTK_WIDGET (data), (gchar *) uri, UI_HTMLVIEW_LAUNCH_DEFAULT);
 		return TRUE;
 	}
 }
