d3f0876ae4ad325a3f11824d2bafbdca2cecfa45
Lars Lindner <lars.lindner@gmail.com>
2007-08-20 22:11:50 +0000

    fix for incorrect double UTF-8 conversion (patch by Vincent Lefevre)

--- src/itemview.c
+++ src/itemview.c
@@ -247,15 +247,12 @@ void itemview_update(void) {
 /* date format handling (not sure if this is the right place) */
 
 gchar * itemview_format_date(time_t date) {
-	gchar		*timestr, *tmp;
+	gchar		*timestr;
 
 	if(itemView_priv.userDefinedDateFmt)
-		tmp = common_format_date(date, itemView_priv.userDefinedDateFmt);
+		timestr = common_format_date(date, itemView_priv.userDefinedDateFmt);
 	else
-		tmp = common_format_nice_date(date);
-	
-	timestr = g_locale_to_utf8(tmp, -1, NULL, NULL, NULL);
-	g_free(tmp);
+		timestr = common_format_nice_date(date);
 	
 	return timestr;
 }
