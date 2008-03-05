2ca5da6e5e5657fdccbee8ef1d46ccf13b3c7a4d
Lars Lindner <lars.lindner@gmail.com>
2008-03-05 21:32:33 +0000

    compilation and copyright fixes...

--- src/migrate.c
+++ src/migrate.c
@@ -1,7 +1,7 @@
 /**
  * @file migrate.c migration between different cache versions
  * 
- * Copyright (C) 2007 Lars Lindner <lars.lindner@gmail.com>
+ * Copyright (C) 2007-2008  Lars Lindner <lars.lindner@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
