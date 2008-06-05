4da7b3330071d9d0a30b3916e77b933e516eccd1
Arnold Noronha <arnstein87@users.sf.net>
2008-06-05 16:32:49 +0000

    r2405@laptop:  arnold | 2008-06-05 22:02:31 +0530  Add GPL and copyright notices.

--- src/fl_sources/google_source_feed.c
+++ src/fl_sources/google_source_feed.c
@@ -1,3 +1,22 @@
+/**
+ * @file google_source_feed.c  Google reader feed subscription routines
+ * 
+ * Copyright (C) 2008 Arnold Noronha <arnstein87@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version. 
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
 
 #include <glib.h>
 #include <string.h>
@@ -11,6 +30,7 @@
 #include "google_source.h"
 #include "subscription.h"
 #include "node.h"
+#include "google_source_edit.h"
 
 /**
  * This is identical to xpath_foreach_match, except that it takes the context
