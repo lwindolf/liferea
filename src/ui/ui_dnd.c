53bddf64c932aee470b518cd435934d43d347e9d
Lars Lindner <lars.lindner@gmail.com>
2008-06-25 22:01:01 +0000

    applying formatting standard

--- src/ui/ui_dnd.c
+++ src/ui/ui_dnd.c
@@ -48,14 +48,16 @@ static gboolean (*old_feed_drag_data_received)(GtkTreeDragDest *drag_dest,
 /* ---------------------------------------------------------------------------- */
 
 /** decides wether a feed cannot be dragged or not */
-static gboolean ui_dnd_feed_draggable(GtkTreeDragSource *drag_source, GtkTreePath *path) {
+static gboolean
+ui_dnd_feed_draggable (GtkTreeDragSource *drag_source, GtkTreePath *path)
+{
 	GtkTreeIter	iter;
 	nodePtr		node;
 	
-	debug1(DEBUG_GUI, "DnD check if feed dragging is possible (%d)", path);
+	debug1 (DEBUG_GUI, "DnD check if feed dragging is possible (%d)", path);
 
-	if(gtk_tree_model_get_iter(GTK_TREE_MODEL(drag_source), &iter, path)) {
-		gtk_tree_model_get(GTK_TREE_MODEL(drag_source), &iter, FS_PTR, &node, -1);
+	if (gtk_tree_model_get_iter (GTK_TREE_MODEL (drag_source), &iter, path)) {
+		gtk_tree_model_get (GTK_TREE_MODEL (drag_source), &iter, FS_PTR, &node, -1);
 		
 		/* never drag "empty" entries or nodes of read-only subscription lists*/
 		if (!node || !(NODE_SOURCE_TYPE (node->parent)->capabilities & NODE_SOURCE_CAPABILITY_WRITABLE_FEEDLIST))
@@ -63,7 +65,7 @@ static gboolean ui_dnd_feed_draggable(GtkTreeDragSource *drag_source, GtkTreePat
 		
 		return TRUE;
 	} else {
-		g_warning("fatal error! could not resolve tree path!");
+		g_warning ("fatal error! could not resolve tree path!");
 		return FALSE;
 	}
 }
@@ -105,54 +107,57 @@ ui_dnd_feed_drop_possible (GtkTreeDragDest *drag_dest, GtkTreePath *dest_path, G
 	return TRUE;
 }
 
-static gboolean ui_dnd_feed_drag_data_received(GtkTreeDragDest *drag_dest, GtkTreePath *dest, GtkSelectionData *selection_data) {
+static gboolean
+ui_dnd_feed_drag_data_received (GtkTreeDragDest *drag_dest, GtkTreePath *dest, GtkSelectionData *selection_data)
+{
 	GtkTreeIter	iter, iter2, parentIter;
 	nodePtr		node, oldParent, newParent;
 	gboolean	result, valid, added;
 	gint		oldPos, pos;
 
-	if(TRUE == (result = old_feed_drag_data_received(drag_dest, dest, selection_data))) {
-		if(gtk_tree_model_get_iter(GTK_TREE_MODEL(drag_dest), &iter, dest)) {
-			gtk_tree_model_get(GTK_TREE_MODEL(drag_dest), &iter, FS_PTR, &node, -1);
+	result = old_feed_drag_data_received (drag_dest, dest, selection_data);
+	if (result) {
+		if (gtk_tree_model_get_iter (GTK_TREE_MODEL (drag_dest), &iter, dest)) {
+			gtk_tree_model_get (GTK_TREE_MODEL (drag_dest), &iter, FS_PTR, &node, -1);
 			
 			/* remove from old parents child list */
 			oldParent = node->parent;
-			g_assert(oldParent);
-			oldPos = g_slist_index(oldParent->children, node);
-			oldParent->children = g_slist_remove(oldParent->children, node);
+			g_assert (oldParent);
+			oldPos = g_slist_index (oldParent->children, node);
+			oldParent->children = g_slist_remove (oldParent->children, node);
 			
-			if(0 == g_slist_length(oldParent->children))
-				ui_node_add_empty_node(ui_node_to_iter(oldParent->id));
+			if (0 == g_slist_length (oldParent->children))
+				ui_node_add_empty_node (ui_node_to_iter (oldParent->id));
 			
 			/* and rebuild new parents child list */
-			if(gtk_tree_model_iter_parent(GTK_TREE_MODEL(drag_dest), &parentIter, &iter)) {
-				gtk_tree_model_get(GTK_TREE_MODEL(drag_dest), &parentIter, FS_PTR, &newParent, -1);
+			if (gtk_tree_model_iter_parent (GTK_TREE_MODEL (drag_dest), &parentIter, &iter)) {
+				gtk_tree_model_get (GTK_TREE_MODEL (drag_dest), &parentIter, FS_PTR, &newParent, -1);
 			} else {
-				gtk_tree_model_get_iter_first(GTK_TREE_MODEL(drag_dest), &parentIter);
-				newParent = feedlist_get_root();
+				gtk_tree_model_get_iter_first (GTK_TREE_MODEL (drag_dest), &parentIter);
+				newParent = feedlist_get_root ();
 			}
 
 			/* drop old list... */
-			debug3(DEBUG_GUI, "old parent is %s (%d, position=%d)", oldParent->title, g_slist_length(oldParent->children), oldPos);
-			debug2(DEBUG_GUI, "new parent is %s (%d)", newParent->title, g_slist_length(newParent->children));
-			g_slist_free(newParent->children);
+			debug3 (DEBUG_GUI, "old parent is %s (%d, position=%d)", oldParent->title, g_slist_length (oldParent->children), oldPos);
+			debug2 (DEBUG_GUI, "new parent is %s (%d)", newParent->title, g_slist_length (newParent->children));
+			g_slist_free (newParent->children);
 			newParent->children = NULL;
 			node->parent = newParent;
 			
-			debug0(DEBUG_GUI, "new new parent child list:");
+			debug0 (DEBUG_GUI, "new newParent child list:");
 				
 			/* and rebuild it from the tree model */
-			if(feedlist_get_root() != newParent)
-				valid = gtk_tree_model_iter_children(GTK_TREE_MODEL(drag_dest), &iter2, &parentIter);
+			if (feedlist_get_root() != newParent)
+				valid = gtk_tree_model_iter_children (GTK_TREE_MODEL (drag_dest), &iter2, &parentIter);
 			else
-				valid = gtk_tree_model_iter_children(GTK_TREE_MODEL(drag_dest), &iter2, NULL);
+				valid = gtk_tree_model_iter_children (GTK_TREE_MODEL (drag_dest), &iter2, NULL);
 				
 			pos = 0;
 			added = FALSE;
-			while(valid) {
+			while (valid) {
 				nodePtr	child;			
-				gtk_tree_model_get(GTK_TREE_MODEL(drag_dest), &iter2, FS_PTR, &child, -1);
-				if(child) {
+				gtk_tree_model_get (GTK_TREE_MODEL (drag_dest), &iter2, FS_PTR, &child, -1);
+				if (child) {
 					/* Well this is a bit complicated... If we move a feed inside a folder
 					   we need to skip the old insertion point (oldPos). This is easy if the
 					   feed is added behind this position. If it is dropped before the flag
@@ -161,58 +166,58 @@ static gboolean ui_dnd_feed_drag_data_received(GtkTreeDragDest *drag_dest, GtkTr
 					 */
 					 
 					/* check if this is a copy of the dragged node or the original itself */
-					if((newParent == oldParent) && !strcmp(node->id, child->id)) {
-						if((pos == oldPos) || added) {
+					if ((newParent == oldParent) && !strcmp(node->id, child->id)) {
+						if ((pos == oldPos) || added) {
 							/* it is the original */
-							debug2(DEBUG_GUI, "   -> %d: skipping old insertion point %s", pos, child->title);
+							debug2 (DEBUG_GUI, "   -> %d: skipping old insertion point %s", pos, child->title);
 						} else {
 							/* it is a copy inserted before the original */
 							added = TRUE;
-							debug2(DEBUG_GUI, "   -> %d: new insertion point of %s", pos, child->title);
-							newParent->children = g_slist_append(newParent->children, child);
+							debug2 (DEBUG_GUI, "   -> %d: new insertion point of %s", pos, child->title);
+							newParent->children = g_slist_append (newParent->children, child);
 						}
 					} else {
 						/* all other nodes */
-						debug2(DEBUG_GUI, "   -> %d: adding %s", pos, child->title);
-						newParent->children = g_slist_append(newParent->children, child);
+						debug2 (DEBUG_GUI, "   -> %d: adding %s", pos, child->title);
+						newParent->children = g_slist_append (newParent->children, child);
 					}
 				} else {
-					debug0(DEBUG_GUI, "   -> removing empty node");
+					debug0 (DEBUG_GUI, "   -> removing empty node");
 					/* remove possible existing "(empty)" node from newParent */
-					ui_node_remove_empty_node(&parentIter);
+					ui_node_remove_empty_node (&parentIter);
 				}
-				valid = gtk_tree_model_iter_next(GTK_TREE_MODEL(drag_dest), &iter2);
+				valid = gtk_tree_model_iter_next (GTK_TREE_MODEL (drag_dest), &iter2);
 				pos++;
 			}
 			
 			node_update_counters (newParent);
 			node_update_counters (oldParent);
 			
-			feedlist_schedule_save();
-			ui_itemlist_prefocus();
+			feedlist_schedule_save ();
+			ui_itemlist_prefocus ();
 		}
 	}
 						      
 	return result;
 }
 
-void ui_dnd_setup_feedlist(GtkTreeStore *feedstore) {
+void
+ui_dnd_setup_feedlist (GtkTreeStore *feedstore)
+{
 	GtkTreeDragSourceIface	*drag_source_iface;
 	GtkTreeDragDestIface	*drag_dest_iface;
 
-	drag_source_iface = GTK_TREE_DRAG_SOURCE_GET_IFACE(GTK_TREE_MODEL(feedstore));
+	drag_source_iface = GTK_TREE_DRAG_SOURCE_GET_IFACE (GTK_TREE_MODEL (feedstore));
 	drag_source_iface->row_draggable = ui_dnd_feed_draggable;
 
-	drag_dest_iface = GTK_TREE_DRAG_DEST_GET_IFACE(GTK_TREE_MODEL(feedstore));
+	drag_dest_iface = GTK_TREE_DRAG_DEST_GET_IFACE (GTK_TREE_MODEL (feedstore));
 	old_feed_drop_possible = drag_dest_iface->row_drop_possible;
 	old_feed_drag_data_received = drag_dest_iface->drag_data_received;
 	drag_dest_iface->row_drop_possible = ui_dnd_feed_drop_possible;
 	drag_dest_iface->drag_data_received = ui_dnd_feed_drag_data_received;
 }
 
-/* ---------------------------------------------------------------------------- */
-/* receiving URLs 								*/
-/* ---------------------------------------------------------------------------- */
+/* receiving URLs */
 
 /* method to receive URLs which were dropped anywhere in the main window */
 static void
@@ -242,8 +247,9 @@ ui_dnd_URL_received (GtkWidget *widget, GdkDragContext *context, gint x, gint y,
 	}
 }
 
-void ui_dnd_setup_URL_receiver(GtkWidget *widget) {
-
+void
+ui_dnd_setup_URL_receiver (GtkWidget *widget)
+{
 	GtkTargetEntry target_table[] = {
 		{ "STRING",     		0, 0 },
 		{ "text/plain", 		0, 0 },
@@ -253,10 +259,10 @@ void ui_dnd_setup_URL_receiver(GtkWidget *widget) {
 	};
 
 	/* doesn't work with GTK_DEST_DEFAULT_DROP... */
-	gtk_drag_dest_set(widget, GTK_DEST_DEFAULT_ALL,
-			target_table, sizeof(target_table)/sizeof(target_table[0]),
-			GDK_ACTION_COPY | GDK_ACTION_MOVE | GDK_ACTION_LINK);
+	gtk_drag_dest_set (widget, GTK_DEST_DEFAULT_ALL,
+	                   target_table, sizeof (target_table)/sizeof (target_table[0]),
+	                   GDK_ACTION_COPY | GDK_ACTION_MOVE | GDK_ACTION_LINK);
 		       
-	gtk_signal_connect(GTK_OBJECT(widget), "drag_data_received",
-			G_CALLBACK(ui_dnd_URL_received), NULL);
+	gtk_signal_connect (GTK_OBJECT (widget), "drag_data_received",
+	                    G_CALLBACK (ui_dnd_URL_received), NULL);
 }
