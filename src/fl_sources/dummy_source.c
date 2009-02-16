044d3071b0831b5ba2d23fd353b2880fbb11bfe5
Adrian Bunk <bunk@users.sourceforge.net>
2009-02-16 17:45:39 +0000

    src/fl_sources/dummy_source.c must #include "fl_sources/dummy_source.h" for seeing the prototype of its own global function

--- src/fl_sources/dummy_source.c
+++ src/fl_sources/dummy_source.c
@@ -18,6 +18,8 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "fl_sources/dummy_source.h"
+
 #include "common.h"
 #include "fl_sources/node_source.h"
 #include "ui/ui_common.h"
