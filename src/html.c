6cecdceff343476956df4b131168233a6325ce49
Lars Lindner <lars.lindner@gmail.com>
2009-02-21 23:09:42 +0000

    Apply code formatting. Remove unnecessary include.

--- src/html.c
+++ src/html.c
@@ -1,8 +1,8 @@
 /**
- * @file html.c HTML file handling / feed auto discovery
+ * @file html.c  HTML favicon and feed link auto discovery
  * 
  * Copyright (C) 2004 ahmed el-helw <ahmedre@cc.gatech.edu>
- * Copyright (C) 2004-2007 Lars Lindner <lars.lindner@gmail.com>
+ * Copyright (C) 2004-2009 Lars Lindner <lars.lindner@gmail.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -18,10 +18,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
 
 #define _GNU_SOURCE
 
@@ -40,68 +36,71 @@ enum {
 	LINK_NORMAL_ANCHOR
 };
 
-static gchar *checkLinkRef(const gchar* str, gint linkType) {
-	gchar	*res;
+static gchar *
+checkLinkRef (const gchar* str, gint linkType)
+{
+	gchar		*res;
 	const gchar	*tmp, *tmp2;
-	size_t	len=0;
-	gchar	quote; 
+	size_t		len = 0;
+	gchar		quote; 
 	
 	/*debug1(DEBUG_PARSING, "checking link %s", str); */
-	tmp = common_strcasestr(str, "href=");
-	if(NULL == tmp) return NULL;
+	tmp = common_strcasestr (str, "href=");
+	if (!tmp)
+		return NULL;
 	tmp += 5;
+	
 	/* skip spaces up to the first quote. This is really slightly
 	 wrong.  SGML allows unquoted atributes, but not if they contain
 	 slashes, so 99% of all URIs will require quotes. */
-	while (*tmp != '\"' &&
-		  *tmp != '\'') {
-		if (*tmp == '>' ||
-		    *tmp == '\0' ||
-		    !isspace(*tmp))
+	while (*tmp != '\"' && *tmp != '\'') {
+		if (*tmp == '>' || *tmp == '\0' || !isspace(*tmp))
 			return NULL;
 		tmp++;
 	}
 	quote = *tmp; /* The type of quote mark used to delimit the arg */
 	tmp++;
 	tmp2 = tmp;
-	while ((*tmp2 != quote && *(tmp2-1) != '\\') &&/* Escaped quote*/
+	while ((*tmp2 != quote && *(tmp2-1) != '\\') && /* Escaped quote*/
 		  *tmp2 != '\0')
 		tmp2++, len++;
 
-	res = g_strndup(tmp, len);
+	res = g_strndup (tmp, len);
 
-	if(linkType == LINK_FAVICON) {
+	if (linkType == LINK_FAVICON) {
 		/* The type attribute is optional, so don't check for it,
 		 * as according to the W3C, it must be must be png, gif or
 		 * ico anyway: http://www.w3.org/2005/10/howto-favicon
 		 * Instead, do stronger checks for the rel attribute. */
-		if(((NULL != common_strcasestr(str, "rel=\"shortcut icon\"")) ||
-		    (NULL != common_strcasestr(str, "rel=\"icon\"")) ||
-		    (NULL != common_strcasestr(str, "rel=\'shortcut icon\'")) ||
-		    (NULL != common_strcasestr(str, "rel=\'icon\'"))) /*&&
-		   ((NULL != common_strcasestr(str, "image/x-icon")) ||
-		    (NULL != common_strcasestr(str, "image/png")) ||
-		    (NULL != common_strcasestr(str, "image/gif")))*/)
+		if (((NULL != common_strcasestr (str, "rel=\"shortcut icon\"")) ||
+		     (NULL != common_strcasestr (str, "rel=\"icon\"")) ||
+		     (NULL != common_strcasestr (str, "rel=\'shortcut icon\'")) ||
+		     (NULL != common_strcasestr (str, "rel=\'icon\'"))) /*&&
+		    ((NULL != common_strcasestr (str, "image/x-icon")) ||
+		     (NULL != common_strcasestr (str, "image/png")) ||
+		     (NULL != common_strcasestr (str, "image/gif")))*/)
 			return res;
-	} else if(linkType == LINK_RSS_ALTERNATE){
-		if((common_strcasestr(str, "alternate")!=NULL) &&
-		   ((common_strcasestr(str, "text/xml")!=NULL) || 
-		    (common_strcasestr(str, "rss+xml")!=NULL) ||
-		    (common_strcasestr(str, "rdf+xml")!=NULL) ||
-		    (common_strcasestr(str, "atom+xml")!=NULL)))
+	} else if (linkType == LINK_RSS_ALTERNATE) {
+		if ((common_strcasestr (str, "alternate") != NULL) &&
+		    ((common_strcasestr (str, "text/xml") != NULL) || 
+		     (common_strcasestr (str, "rss+xml") != NULL) ||
+		     (common_strcasestr (str, "rdf+xml") != NULL) ||
+		     (common_strcasestr (str, "atom+xml") != NULL)))
 			return res;
-	} else if(linkType == LINK_NORMAL_ANCHOR){
-		if((strstr(res, "rdf")) || 
-		   (strstr(res, "xml")) ||
-		   (strstr(res, "rss")) ||
-		   (strstr(res, "atom")))
+	} else if (linkType == LINK_NORMAL_ANCHOR) {
+		if ((strstr (res, "rdf")) || 
+		    (strstr (res, "xml")) ||
+		    (strstr (res, "rss")) ||
+		    (strstr (res, "atom")))
 			return res;
 	}
-	g_free(res);
+	g_free (res);
 	return NULL;
 }
 
-static gchar *search_links(const gchar* data, gint linkType) {
+static gchar *
+search_links (const gchar* data, gint linkType)
+{
 	gchar	*ptr;
 	const gchar	*tmp = data;
 	gchar	*result = NULL;
@@ -109,20 +108,20 @@ static gchar *search_links(const gchar* data, gint linkType) {
 	gchar	*tstr;
 	gchar	*endptr;
 	
-	while(1) {
-		ptr = common_strcasestr(tmp, ((linkType != LINK_NORMAL_ANCHOR)? "<link " : "<a "));
-		if(NULL == ptr)
+	while (1) {
+		ptr = common_strcasestr (tmp, ((linkType != LINK_NORMAL_ANCHOR)? "<link " : "<a "));
+		if (!ptr)
 			break;
 		
-		endptr = strchr(ptr, '>');
+		endptr = strchr (ptr, '>');
 		if (!endptr)
 			break;
 		*endptr = '\0';
-		tstr = g_strdup(ptr);
+		tstr = g_strdup (ptr);
 		*endptr = '>';
-		res = checkLinkRef(tstr, linkType);
-		g_free(tstr);
-		if(res != NULL){
+		res = checkLinkRef (tstr, linkType);
+		g_free (tstr);
+		if (res) {
 			result = res;
 			break;
 /*		deactivated as long as we support only subscribing 
@@ -141,44 +140,48 @@ static gchar *search_links(const gchar* data, gint linkType) {
 		}
 		tmp = endptr;
 	}
-	result = unhtmlize(result); /* URIs can contain escaped things.... All ampersands must be escaped, for example */
+	result = unhtmlize (result); /* URIs can contain escaped things.... All ampersands must be escaped, for example */
 	return result;
 }
 
-gchar * html_auto_discover_feed(const gchar* data, const gchar *baseUri) {
+gchar *
+html_auto_discover_feed (const gchar* data, const gchar *baseUri)
+{
 	gchar	*res, *tmp;
 
-	debug0(DEBUG_UPDATE, "searching through link tags");
-	res = search_links(data, LINK_RSS_ALTERNATE);
-	debug1(DEBUG_UPDATE, "search result: %s", res? res : "none found");
-	if(res == NULL) {
-		debug0(DEBUG_UPDATE, "searching through href tags");
-		res = search_links(data, LINK_NORMAL_ANCHOR);
-		debug1(DEBUG_UPDATE, "search result: %s", res? res : "none found");
+	debug0 (DEBUG_UPDATE, "searching through link tags");
+	res = search_links (data, LINK_RSS_ALTERNATE);
+	debug1 (DEBUG_UPDATE, "search result: %s", res?res:"none found");
+	if (!res) {
+		debug0 (DEBUG_UPDATE, "searching through href tags");
+		res = search_links (data, LINK_NORMAL_ANCHOR);
+		debug1 (DEBUG_UPDATE, "search result: %s", res?res:"none found");
 	}
 
-	if(res) {
+	if (res) {
 		/* turn relative URIs into absolute URIs */
 		tmp = res;
-		res = common_build_url(res, baseUri);
-		g_free(tmp);
+		res = common_build_url (res, baseUri);
+		g_free (tmp);
 	}
 
 	return res;
 }
 
-gchar * html_discover_favicon(const gchar* data, const gchar *baseUri) {
+gchar *
+html_discover_favicon (const gchar * data, const gchar * baseUri)
+{
 	gchar	*res, *tmp;
 
-	debug0(DEBUG_UPDATE, "searching through link tags");
-	res = search_links(data, LINK_FAVICON);
-	debug1(DEBUG_UPDATE, "search result: %s", res? res : "none found");
+	debug0 (DEBUG_UPDATE, "searching through link tags");
+	res = search_links (data, LINK_FAVICON);
+	debug1 (DEBUG_UPDATE, "search result: %s", res? res : "none found");
 
-	if (res != NULL) {
+	if (res) {
 		/* turn relative URIs into absolute URIs */
 		tmp = res;
-		res = common_build_url(res, baseUri);
-		g_free(tmp);
+		res = common_build_url (res, baseUri);
+		g_free (tmp);
 	}
 	
 	return res;
