d29b52516882ec63b399205e63836dee423a6b5e
Lars Lindner <lars.lindner@gmail.com>
2009-02-25 23:53:20 +0000

    Applying formatting style. Adding XSLT concept explanation.

--- src/render.c
+++ src/render.c
@@ -43,83 +43,106 @@
 #include "ui/itemview.h"
 #include "ui/liferea_shell.h"
 
+/* Liferea provides special screens and the item and the feed displays
+   using self-generated HTML. To separate code and layout and to easily
+   localize the layout it is provided by automake XSL stylesheet templates.
+   
+   Using automake translations are merged into those XSL stylesheets. On
+   startup Liferea loads those expanded XSL stylesheets. During startup
+   Liferea initially reduces the contained translations to the currently
+   used ones by stripping all others from the XSL stylesheet using the
+   localization stylesheet (xslt/i18n-filter.xslt). The resulting XSLT
+   instance is kept in memory and used to render each items and feeds. 
+   
+   The following code uses a hash table to maintain stylesheet instance
+   and performs CSS adaptions to the current GTK theme. */
+
 static renderParamPtr	langParams = NULL;	/* the current locale settings (for localization stylesheet) */
 static gchar		*defaultParams = NULL;	/* some default parameters (for rendering stylesheets) */
 
 static GHashTable	*stylesheets = NULL;	/* XSLT stylesheet cache */
 
-static void render_init(void) {
+static void
+render_init (void)
+{
 	gchar   **shortlang = NULL;	/* e.g. "de" */
 	gchar	**lang = NULL;		/* e.g. "de_AT" */
 
-	if(langParams)
-		render_parameter_free(langParams);
-	if(defaultParams)
-		g_free(defaultParams);
+	if (langParams)
+		render_parameter_free (langParams);
+	if (defaultParams)
+		g_free (defaultParams);
 
 	/* prepare localization parameters */
-	debug1(DEBUG_HTML, "XSLT localisation: setlocale(LC_MESSAGES, NULL) reports '%s'", setlocale(LC_MESSAGES, NULL));
-	lang = g_strsplit(setlocale(LC_MESSAGES, NULL), "@", 0);
-	shortlang = g_strsplit(setlocale(LC_MESSAGES, NULL), "_", 0);
+	debug1 (DEBUG_HTML, "XSLT localisation: setlocale(LC_MESSAGES, NULL) reports '%s'", setlocale(LC_MESSAGES, NULL));
+	lang = g_strsplit (setlocale (LC_MESSAGES, NULL), "@", 0);
+	shortlang = g_strsplit (setlocale (LC_MESSAGES, NULL), "_", 0);
 
-	langParams = render_parameter_new();
-	render_parameter_add(langParams, "lang='%s'", lang[0]);
-	render_parameter_add(langParams, "shortlang='%s'", shortlang[0]);
-	debug2(DEBUG_HTML, "XSLT localisation: lang='%s' shortlang='%s'", lang[0], shortlang[0]);
+	langParams = render_parameter_new ();
+	render_parameter_add (langParams, "lang='%s'", lang[0]);
+	render_parameter_add (langParams, "shortlang='%s'", shortlang[0]);
+	debug2 (DEBUG_HTML, "XSLT localisation: lang='%s' shortlang='%s'", lang[0], shortlang[0]);
 
-	g_strfreev(shortlang);
-	g_strfreev(lang);
+	g_strfreev (shortlang);
+	g_strfreev (lang);
 
 	/* prepare rendering default parameters */
 	defaultParams = g_strdup_printf("search_link_enable='%s'", conf_get_bool_value (SOCIAL_LINK_SEARCH_HIDE)?"false":"true");
 
-	if(!stylesheets)
+	if (!stylesheets)
 		stylesheets = g_hash_table_new_full(g_str_hash, g_str_equal, g_free, NULL);
 }
 
-static xsltStylesheetPtr render_load_stylesheet(const gchar *xsltName) {
+static xsltStylesheetPtr
+render_load_stylesheet (const gchar *xsltName)
+{
 	xsltStylesheetPtr	i18n_filter;
 	xsltStylesheetPtr	xslt;
 	xmlDocPtr		xsltDoc, resDoc;
 	gchar			*filename;
 
-	if(!stylesheets)
-		render_init();
+	if (!stylesheets)
+		render_init ();
 
 	/* try to serve the stylesheet from the cache */
-	xslt = (xsltStylesheetPtr)g_hash_table_lookup(stylesheets, xsltName);
-	if(xslt)
+	xslt = (xsltStylesheetPtr)g_hash_table_lookup (stylesheets, xsltName);
+	if (xslt)
 		return xslt;
 
 	/* or load and translate it... */
 
 	/* 1. load localization stylesheet */
-	if (NULL == (i18n_filter = xsltParseStylesheetFile(PACKAGE_DATA_DIR G_DIR_SEPARATOR_S PACKAGE G_DIR_SEPARATOR_S "xslt" G_DIR_SEPARATOR_S "i18n-filter.xslt"))) {
-		g_warning("fatal: could not load localization stylesheet!");
+	i18n_filter = xsltParseStylesheetFile (PACKAGE_DATA_DIR G_DIR_SEPARATOR_S PACKAGE G_DIR_SEPARATOR_S "xslt" G_DIR_SEPARATOR_S "i18n-filter.xslt");
+	if (!i18n_filter) {
+		g_warning ("fatal: could not load localization stylesheet!");
 		return NULL;
 	}
 
 	/* 2. load and localize the rendering stylesheet */
-	filename = g_strjoin(NULL, PACKAGE_DATA_DIR G_DIR_SEPARATOR_S PACKAGE G_DIR_SEPARATOR_S "xslt" G_DIR_SEPARATOR_S, xsltName, ".xml", NULL);
+	filename = g_strjoin (NULL, PACKAGE_DATA_DIR G_DIR_SEPARATOR_S PACKAGE G_DIR_SEPARATOR_S "xslt" G_DIR_SEPARATOR_S, xsltName, ".xml", NULL);
+	xsltDoc = xmlParseFile (filename);
+	if (!xsltDoc)
+		g_warning ("fatal: could not load rendering stylesheet (%s)!", xsltName);
 
-	if (NULL == (xsltDoc = xmlParseFile(filename)))
-		g_warning("fatal: could not load rendering stylesheet (%s)!", xsltName);
+	g_free (filename);
 
-	g_free(filename);
+	resDoc = xsltApplyStylesheet (i18n_filter, xsltDoc, (const gchar **)langParams->params);
+	if (!resDoc)
+		g_warning ("fatal: applying localization stylesheet failed (%s)!", xsltName);
 
-	if(NULL == (resDoc = xsltApplyStylesheet(i18n_filter, xsltDoc, (const gchar **)langParams->params)))
-		g_warning("fatal: applying localization stylesheet failed (%s)!", xsltName);
-
-	//xsltSaveResultToFile(stdout, resDoc, i18n_filter);
+	/* Use the following to debug XSLT transformation problems */
+	/* xsltSaveResultToFile (stdout, resDoc, i18n_filter); */
 
 	/* 3. create localized rendering stylesheet */
-	if(NULL == (xslt = xsltParseStylesheetDoc(resDoc)))
+	xslt = xsltParseStylesheetDoc(resDoc);
+	if (!xslt)
 		g_warning("fatal: could not load rendering stylesheet (%s)!", xsltName);
 
-	xmlFreeDoc(xsltDoc);
-	xsltFreeStylesheet(i18n_filter);
+	xmlFreeDoc (xsltDoc);
+	xsltFreeStylesheet (i18n_filter);
 
-	g_hash_table_insert(stylesheets, g_strdup(xsltName), xslt);
+	g_hash_table_insert (stylesheets, g_strdup (xsltName), xslt);
+	
 	return xslt;
 }
 
@@ -128,7 +151,6 @@ static GString	*css = NULL;
 static time_t lastCssModification = 0;
 
 /** widget background theme colors as 8bit HTML RGB code */
-
 typedef struct themeColor {
 	const gchar	*name;
 	gchar		*value;
@@ -137,7 +159,9 @@ typedef struct themeColor {
 static GSList *themeColors = NULL;
 
 /* Determining of theme colors, to be inserted in CSS */
-static themeColorPtr render_get_theme_color(const gchar *name, GdkColor themeColor) {
+static themeColorPtr
+render_get_theme_color (const gchar *name, GdkColor themeColor)
+{
 	themeColorPtr	tc;
 	gushort		r, g, b;
 
@@ -145,10 +169,10 @@ static themeColorPtr render_get_theme_color(const gchar *name, GdkColor themeCol
 	g = themeColor.green / 256;
 	b = themeColor.blue / 256;
 
-	tc = g_new0(struct themeColor, 1);
+	tc = g_new0 (struct themeColor, 1);
 	tc->name = name;
-	tc->value = g_strdup_printf("%.2X%.2X%.2X", r, g, b);
-	debug2(DEBUG_HTML, "theme color \"%s\" is %s", tc->name, tc->value);
+	tc->value = g_strdup_printf ("%.2X%.2X%.2X", r, g, b);
+	debug2 (DEBUG_HTML, "theme color \"%s\" is %s", tc->name, tc->value);
 
 	return tc;
 }
@@ -162,13 +186,13 @@ render_get_theme_colors (void)
 	style = itemview_get_style ();
 
 	g_assert (NULL == themeColors);
-	themeColors = g_slist_append (themeColors, render_get_theme_color("GTK-COLOR-FG",    style->fg[GTK_STATE_NORMAL]));
-	themeColors = g_slist_append (themeColors, render_get_theme_color("GTK-COLOR-BG",    style->bg[GTK_STATE_NORMAL]));
-	themeColors = g_slist_append (themeColors, render_get_theme_color("GTK-COLOR-LIGHT", style->light[GTK_STATE_NORMAL]));
-	themeColors = g_slist_append (themeColors, render_get_theme_color("GTK-COLOR-DARK",  style->dark[GTK_STATE_NORMAL]));
-	themeColors = g_slist_append (themeColors, render_get_theme_color("GTK-COLOR-MID",   style->mid[GTK_STATE_NORMAL]));
-	themeColors = g_slist_append (themeColors, render_get_theme_color("GTK-COLOR-BASE",  style->base[GTK_STATE_NORMAL]));
-	themeColors = g_slist_append (themeColors, render_get_theme_color("GTK-COLOR-TEXT",  style->text[GTK_STATE_NORMAL]));
+	themeColors = g_slist_append (themeColors, render_get_theme_color ("GTK-COLOR-FG",    style->fg[GTK_STATE_NORMAL]));
+	themeColors = g_slist_append (themeColors, render_get_theme_color ("GTK-COLOR-BG",    style->bg[GTK_STATE_NORMAL]));
+	themeColors = g_slist_append (themeColors, render_get_theme_color ("GTK-COLOR-LIGHT", style->light[GTK_STATE_NORMAL]));
+	themeColors = g_slist_append (themeColors, render_get_theme_color ("GTK-COLOR-DARK",  style->dark[GTK_STATE_NORMAL]));
+	themeColors = g_slist_append (themeColors, render_get_theme_color ("GTK-COLOR-MID",   style->mid[GTK_STATE_NORMAL]));
+	themeColors = g_slist_append (themeColors, render_get_theme_color ("GTK-COLOR-BASE",  style->base[GTK_STATE_NORMAL]));
+	themeColors = g_slist_append (themeColors, render_get_theme_color ("GTK-COLOR-TEXT",  style->text[GTK_STATE_NORMAL]));
 
 	color = NULL;
 	gtk_widget_style_get (liferea_shell_get_window (), "link-color", &color, NULL);
@@ -187,13 +211,15 @@ render_get_theme_colors (void)
 	}
 }
 
-static gchar * render_set_theme_colors(gchar *css) {
+static gchar *
+render_set_theme_colors (gchar *css)
+{
 	GSList	*iter = themeColors;
 	
-	while(iter) {
+	while (iter) {
 		themeColorPtr tc = (themeColorPtr)iter->data;
-		css = common_strreplace(css, tc->name, tc->value);
-		iter = g_slist_next(iter);
+		css = common_strreplace (css, tc->name, tc->value);
+		iter = g_slist_next (iter);
 	}
 	
 	return css;
@@ -333,37 +359,41 @@ render_get_css (gboolean externalCss)
 	return css->str;
 }
 
-gchar * render_xml(xmlDocPtr doc, const gchar *xsltName, renderParamPtr paramSet) {
+gchar *
+render_xml (xmlDocPtr doc, const gchar *xsltName, renderParamPtr paramSet)
+{
 	gchar			*output = NULL;
 	xmlDocPtr		resDoc;
 	xsltStylesheetPtr	xslt;
 	xmlOutputBufferPtr	buf;
 	
-	if(NULL == (xslt = render_load_stylesheet(xsltName)))
+	xslt = render_load_stylesheet(xsltName);
+	if (!xslt)
 		return NULL;
 
-	if(!paramSet)
-		paramSet = render_parameter_new();
-	render_parameter_add(paramSet, "%s", defaultParams);
+	if (!paramSet)
+		paramSet = render_parameter_new ();
+	render_parameter_add (paramSet, "%s", defaultParams);
 
-	if(NULL == (resDoc = xsltApplyStylesheet(xslt, doc, (const gchar **)paramSet->params))) {
-		g_warning("fatal: applying rendering stylesheet (%s) failed!", xsltName);
+	resDoc = xsltApplyStylesheet(xslt, doc, (const gchar **)paramSet->params);
+	if (!resDoc) {
+		g_warning ("fatal: applying rendering stylesheet (%s) failed!", xsltName);
 		return NULL;
 	}
 	
 	/* for debugging use: xsltSaveResultToFile(stdout, resDoc, xslt); */
 	
 	/* save results into return string */
-	buf = xmlAllocOutputBuffer(NULL);
-	if(-1 == xsltSaveResultTo(buf, resDoc, xslt))
-		g_warning("fatal: retrieving result of rendering stylesheet failed (%s)!", xsltName);
+	buf = xmlAllocOutputBuffer (NULL);
+	if (-1 == xsltSaveResultTo(buf, resDoc, xslt))
+		g_warning ("fatal: retrieving result of rendering stylesheet failed (%s)!", xsltName);
 		
-	if(xmlBufferLength(buf->buffer) > 0)
-		output = xmlCharStrdup(xmlBufferContent(buf->buffer));
+	if (xmlBufferLength (buf->buffer) > 0)
+		output = xmlCharStrdup (xmlBufferContent(buf->buffer));
 
-	xmlOutputBufferClose(buf);
-	xmlFreeDoc(resDoc);
-	render_parameter_free(paramSet);
+	xmlOutputBufferClose (buf);
+	xmlFreeDoc (resDoc);
+	render_parameter_free (paramSet);
 	
 	if (output) {
 		gchar *tmp;
@@ -376,13 +406,13 @@ gchar * render_xml(xmlDocPtr doc, const gchar *xsltName, renderParamPtr paramSet
 		}
 	
 		/* Return only the body contents */
-		tmp = strstr(output, "<body>");
-		if(tmp) {
-			tmp = g_strdup(tmp + 6);
-			xmlFree(output);
+		tmp = strstr (output, "<body>");
+		if (tmp) {
+			tmp = g_strdup (tmp + 6);
+			xmlFree (output);
 			output = tmp;
-			tmp = strstr(output, "</body>");
-			if(tmp)
+			tmp = strstr (output, "</body>");
+			if (tmp)
 				*tmp = 0;
 		}
 	}
@@ -390,56 +420,63 @@ gchar * render_xml(xmlDocPtr doc, const gchar *xsltName, renderParamPtr paramSet
 	return output;
 }
 
-gchar * render_file(const gchar *filename, const gchar *xsltName, renderParamPtr paramSet) {
+gchar *
+render_file (const gchar *filename, const gchar *xsltName, renderParamPtr paramSet)
+{
 	xmlDocPtr	srcDoc;
 	gchar		*output;
 
-	if(NULL == (srcDoc = xmlParseFile(filename))) {
-		g_warning("fatal: loading source XML (%s) failed", filename);
+	srcDoc = xmlParseFile(filename);
+	if (!srcDoc) {
+		g_warning ("fatal: loading source XML (%s) failed", filename);
 		return NULL;
 	}
 
-	output = render_xml(srcDoc, xsltName, paramSet);
-	xmlFreeDoc(srcDoc);
+	output = render_xml (srcDoc, xsltName, paramSet);
+	xmlFreeDoc (srcDoc);
 
 	return output;
 }
 
 /* parameter handling */
 
-renderParamPtr render_parameter_new(void) {
-
-	return g_new0(struct renderParam, 1);
+renderParamPtr
+render_parameter_new (void)
+{
+	return g_new0 (struct renderParam, 1);
 }
 
-void render_parameter_add(renderParamPtr paramSet, const gchar *fmt, ...) {
+void
+render_parameter_add (renderParamPtr paramSet, const gchar *fmt, ...)
+{
 	gchar	*new, *value, *name;
 	va_list args;
 	
-	g_assert(NULL != fmt);
-	g_assert(NULL != paramSet);
+	g_assert (NULL != fmt);
+	g_assert (NULL != paramSet);
 	
 	va_start (args, fmt);
 	new = g_strdup_vprintf (fmt, args);
 	va_end (args);
 
 	name = new;
-	value = strchr(new, '=');
-	g_assert(NULL != value);
+	value = strchr (new, '=');
+	g_assert (NULL != value);
 	*value = 0;
 	value++;
 	
 	paramSet->len += 2;
-	paramSet->params = (gchar **)g_realloc(paramSet->params, (paramSet->len + 1)*sizeof(gchar *));
+	paramSet->params = (gchar **)g_realloc (paramSet->params, (paramSet->len + 1)*sizeof(gchar *));
 	paramSet->params[paramSet->len] = NULL;
-	paramSet->params[paramSet->len-2] = g_strdup(name);
-	paramSet->params[paramSet->len-1] = g_strdup(value);
+	paramSet->params[paramSet->len-2] = g_strdup (name);
+	paramSet->params[paramSet->len-1] = g_strdup (value);
 
-	g_free(new);
+	g_free (new);
 }
 
-void render_parameter_free(renderParamPtr paramSet) {
-
-	g_strfreev(paramSet->params);
-	g_free(paramSet);
+void
+render_parameter_free (renderParamPtr paramSet)
+{
+	g_strfreev (paramSet->params);
+	g_free (paramSet);
 }
