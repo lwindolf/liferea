ea79d0a3e6ce6d294c462f7ea7f10669d14986ca
Adrian Bunk <bunk@users.sourceforge.net>
2009-03-15 08:48:05 +0000

    we must ui_popup_update_menues() in social_set_bookmark_site() for getting the change immediately

--- src/social.c
+++ src/social.c
@@ -21,6 +21,7 @@
 #include "conf.h"
 #include "debug.h"
 #include "social.h"
+#include "ui/ui_popup.h"
 
 /** list of registered bookmarking sites */
 GSList *bookmarkSites = NULL;
@@ -66,6 +67,7 @@ social_set_bookmark_site (const gchar *name)
 		bookmarkSite = iter->data;
 		if (g_str_equal (bookmarkSite->name, name)) {
 			conf_set_str_value (SOCIAL_BM_SITE, name);
+			ui_popup_update_menues ();
 			return;
 		}
 		iter = g_slist_next (iter);
