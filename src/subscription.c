a802f858ea9fde9aec78356de7da116b4ed4064d
Arnold Noronha <arnstein87@users.sf.net>
2009-07-07 03:12:24 +0000

    Fixes a breakage in the way Google Source worked due to commit r5045.
    This commit does assume that the subscription->updateJob is valid, which
    seems to be a reasonable assumption. GoogleSource uses requests with
    URLs different from the subscription->source for usability reasons; r5045
    would force the subscription->source to this temporary URL.

--- src/subscription.c
+++ src/subscription.c
@@ -205,7 +205,7 @@ subscription_process_update_result (const struct updateResult * const result, gp
 	/* 1. preprocessing */
 
 	/* update the subscription URL on permanent redirects */
-	if (result->source && !g_str_equal (result->source, subscription_get_source (subscription))) {
+	if (result->source && !g_str_equal (result->source, subscription->updateJob->request->source)) {
 		debug2 (DEBUG_UPDATE, "The URL of \"%s\" has changed permanently and was updated with \"%s\"", node_get_title(node), result->source);
 		subscription_set_source (subscription, result->source);
 		liferea_shell_set_status_bar (_("The URL of \"%s\" has changed permanently and was updated"), node_get_title(node));
