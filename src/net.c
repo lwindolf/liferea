5a8988a44cfdb510ee5c2309310cdd43ec908830
Lars Lindner <lars.lindner@gmail.com>
2009-10-10 14:27:09 +0000

    Added a timeout for downloads.
    Previously you could permanently hang the queue if you download five
    URLs that don't respond.
    
    todo: The error shown if a timeout happens isn't very useful:
    HTTP error code 0: There was an internal error in the update process

--- src/net.c
+++ src/net.c
@@ -209,6 +209,7 @@ network_init (void)
 
 	/* Initialize libsoup */
 	session = soup_session_async_new_with_options (SOUP_SESSION_USER_AGENT, useragent,
+	                                               SOUP_SESSION_TIMEOUT, 120,
 						       SOUP_SESSION_IDLE_TIMEOUT, 30,
 						       SOUP_SESSION_PROXY_URI, proxy,
 						       SOUP_SESSION_ADD_FEATURE, cookies,
@@ -216,6 +217,7 @@ network_init (void)
 
 	/* This session is for those cases where we are told not to use the proxy */
 	session_no_proxy = soup_session_async_new_with_options (SOUP_SESSION_USER_AGENT, useragent,
+	                                                        SOUP_SESSION_TIMEOUT, 120,
 								SOUP_SESSION_IDLE_TIMEOUT, 30,
 								SOUP_SESSION_ADD_FEATURE, cookies,
 								NULL);
@@ -223,6 +225,7 @@ network_init (void)
 	/* This session is for those cases where we need to add specific cookies, e.g. Google Reader.
 	 * Once GNOME #574571 is fixed, we will be able to use the normal session */
 	session_no_cookies = soup_session_async_new_with_options (SOUP_SESSION_USER_AGENT, useragent,
+	                                                          SOUP_SESSION_TIMEOUT, 120,
 								  SOUP_SESSION_IDLE_TIMEOUT, 30,
 								  SOUP_SESSION_PROXY_URI, proxy,
 								  NULL);
@@ -230,6 +233,7 @@ network_init (void)
 	/* And this one is for cases where we need to use our own cookies, and bypass the proxy, e.g.
 	 * Google Reader subscription where the "ignore proxy" preference is set */
 	session_no_cookies_no_proxy = soup_session_async_new_with_options (SOUP_SESSION_USER_AGENT, useragent,
+	                                                                   SOUP_SESSION_TIMEOUT, 120,
 									   SOUP_SESSION_IDLE_TIMEOUT, 30,
 									   NULL);
 
