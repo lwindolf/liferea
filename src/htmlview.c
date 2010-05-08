b906389384af75ab7f9bce4cee206080221dd01c
Lars Lindner <lars.lindner@gmail.com>
2010-05-08 21:43:56 +0000

    Removes unnecessary includes.

--- src/htmlview.c
+++ src/htmlview.c
@@ -17,10 +17,6 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
- 
-#ifdef HAVE_CONFIG_H
-#  include <config.h>
-#endif
 
 #include <string.h>
 #include <libxml/uri.h>
@@ -32,7 +28,6 @@
 #include "htmlview.h"
 #include "item.h"
 #include "itemlist.h"
-#include "itemset.h"
 #include "render.h"
 #include "vfolder.h"
 #include "ui/liferea_htmlview.h"
