347a12a27f1fbad32941c57f7c0cc085e97af8df
Emilio Pozuelo Monfort <pochu27@gmail.com>
2011-02-19 16:12:05 +0000

    gtk_scrollable_set_vadjustment is GTK+ 3 only

--- src/ui/item_list_view.c
+++ src/ui/item_list_view.c
@@ -21,6 +21,10 @@
 
 #include "ui/item_list_view.h"
 
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <string.h>
 #include <glib.h>
 #include <gdk/gdkkeysyms.h>
@@ -314,8 +318,11 @@ item_list_view_clear (ItemListView *ilv)
 	
 	adj = gtk_tree_view_get_vadjustment (ilv->priv->treeview);
 	gtk_adjustment_set_value (adj, 0.0);
+#if GTK_API_VERSION >= 3
 	gtk_scrollable_set_vadjustment (GTK_SCROLLABLE (ilv->priv->treeview), adj);
-
+#else
+	gtk_tree_view_set_vadjustment (ilv->priv->treeview, adj);
+#endif
 	if (itemstore)
 		gtk_tree_store_clear (itemstore);
 	if (ilv->priv->item_id_to_iter)
