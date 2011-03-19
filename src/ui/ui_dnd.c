lwindolf/liferea

commit 04d5b2b0758e40bb79ff25c2ace1324cdaf4bfda

Added FIXME to DnD code to avoid dropping folders into Google Reader.

src/ui/ui_dnd.c
@@ -96,10 +96,17 @@ ui_dnd_feed_drop_possible (GtkTreeDragDest *drag_dest, GtkTreePath *dest_path, G
 	   if it is a writeable node source. */
 
 	gtk_tree_model_get (GTK_TREE_MODEL (drag_dest), &iter, FS_PTR, &node, -1);
-
-	/* never drag nodes of read-only subscription lists */
-	if (node && !(NODE_SOURCE_TYPE (node)->capabilities & NODE_SOURCE_CAPABILITY_WRITABLE_FEEDLIST))
+	if (!node)
 		return FALSE;
+
+	/* never drop into read-only subscription node sources */
+	if (NODE_SOURCE_TYPE (node)->capabilities & NODE_SOURCE_CAPABILITY_WRITABLE_FEEDLIST)
+		return FALSE;
+		
+	/* never drag folders into non-hierarchic node sources */
+// FIXME: How can we implement this=	
+//	if (IS_FOLDER() && !(NODE_SOURCE_TYPE (node)->capabilities & NODE_SOURCE_CAPABILITY_HIERARCHIC_FEEDLIST))
+//		return FALSE;
 
 	return TRUE;
 }

commit df50568fd7922d78256843ddf61bb0da89fc1c07

Fixed drag and drop in the feed list
This is a regression from 04d5b2b0758e40bb79ff25c2ace1324cdaf4bfda

src/ui/ui_dnd.c
@@ -100,11 +100,11 @@ ui_dnd_feed_drop_possible (GtkTreeDragDest *drag_dest, GtkTreePath *dest_path, G
 		return FALSE;
 
 	/* never drop into read-only subscription node sources */
-	if (NODE_SOURCE_TYPE (node)->capabilities & NODE_SOURCE_CAPABILITY_WRITABLE_FEEDLIST)
+	if (!(NODE_SOURCE_TYPE (node)->capabilities & NODE_SOURCE_CAPABILITY_WRITABLE_FEEDLIST))
 		return FALSE;
-		
+
 	/* never drag folders into non-hierarchic node sources */
-// FIXME: How can we implement this=	
+// FIXME: How can we implement this?
 //	if (IS_FOLDER() && !(NODE_SOURCE_TYPE (node)->capabilities & NODE_SOURCE_CAPABILITY_HIERARCHIC_FEEDLIST))
 //		return FALSE;
 