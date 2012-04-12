afe203a84ac44af160be774a8dc8c18367b3771c
Lars Lindner <lars.lindner@gmail.com>
2012-04-12 20:07:39 +0200

    SF #3515880: missing include when compiling with libindicate (patch by Chow Loong Jin)

--- src/ui/ui_indicator.c
+++ src/ui/ui_indicator.c
@@ -18,6 +18,10 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor Boston, MA 02110-1301,  USA
  */
 
+#ifdef HAVE_CONFIG_H
+#   include <config.h>
+#endif
+
 #include "ui_indicator.h"
 
 #ifdef HAVE_LIBINDICATE
