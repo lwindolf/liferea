0ed1079fe3926d31320566eabfbf444230e4f8aa
Lars Lindner <lars.lindner@gmail.com>
2012-07-07 00:54:49 +0200

    Make password store and fetch work

--- plugins/gnome-keyring.py
+++ plugins/gnome-keyring.py
@@ -16,8 +16,8 @@ class GnomeKeyringPlugin(GObject.Object, Liferea.AuthActivatable):
 	# Dump all passwords for debugging:
 	#
 	#print "=== dump list on activate"
-	#(result, items) = GnomeKeyring.list_item_ids_sync("liferea")
-        #for id in items:	
+	#(result, ids) = GnomeKeyring.list_item_ids_sync("liferea")
+        #for id in ids:	
         #   (result, item) = GnomeKeyring.item_get_info_sync("liferea", id)
         #   if result != GnomeKeyring.Result.OK:
 	#      print '%s is locked!' % (id)
@@ -33,15 +33,29 @@ class GnomeKeyringPlugin(GObject.Object, Liferea.AuthActivatable):
         window = self.object
 
     def do_query(self, id):
-	#item = GnomeKeyring.item_get_info_sync("liferea", id)
-        print "query id = %s" % id
-	Liferea.auth_info_from_store(id, "test", "pwd")
+        # Fetch secret by id
+        attrs = GnomeKeyring.Attribute.list_new()
+        GnomeKeyring.Attribute.list_append_string(attrs, 'id', id)
+        result, value = GnomeKeyring.find_items_sync(GnomeKeyring.ItemType.GENERIC_SECRET, attrs)
+        if result != GnomeKeyring.Result.OK:
+          return
+
+	#print 'password %s = %s' % (id, value[0].secret)
+	#print 'password id = %s' % value[0].item_id
+
+	username, password = value[0].secret.split('@@@')
+  	Liferea.auth_info_from_store(id, username, password)
+
+    def do_delete(self, id):
+        keyring = GnomeKeyring.get_default_keyring_sync()[1]
+        GnomeKeyring.item_delete_sync(keyring, id)
 
     def do_store(self, id, username, password):
 	GnomeKeyring.create_sync("liferea", None)
         attrs = GnomeKeyring.Attribute.list_new()
-        GnomeKeyring.Attribute.list_append_string(attrs, 'username', username)
-	GnomeKeyring.item_create_sync("liferea", GnomeKeyring.ItemType.GENERIC_SECRET, repr(id), attrs, repr(password), True)
+        GnomeKeyring.Attribute.list_append_string(attrs, 'id', id)
+        GnomeKeyring.Attribute.list_append_string(attrs, 'user', username)
+	GnomeKeyring.item_create_sync("liferea", GnomeKeyring.ItemType.GENERIC_SECRET, repr(id), attrs, repr('@@@'.join([username, password])), True)
 
 class GnomeKeyringConfigurable(GObject.Object, PeasGtk.Configurable):
     __gtype_name__ = 'GnomeKeyringConfigurable'
