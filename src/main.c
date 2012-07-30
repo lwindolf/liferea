eaf51abeaa3f304bdb7b2b3eda68bc08837eafc0
Lars Windolf <lars.lindner@gmail.com>
2012-07-30 23:43:31 +0200

    Dropping SIGSEGV signal handler to allow distro crash report tools to work (e.g. Ubuntu)

--- src/main.c
+++ src/main.c
@@ -95,23 +95,6 @@ message_received_cb (UniqueApp         *app,
 	return res;
 }
 
-static void G_GNUC_NORETURN
-fatal_signal_handler (int sig)
-{
-	sigset_t sigset;
-
-	sigemptyset (&sigset);
-	sigprocmask (SIG_SETMASK, &sigset, NULL);
-
-	g_print ("\nLiferea did receive signal %d (%s).\n", sig, g_strsignal (sig));
-
-	g_print ("You have probably triggered a program bug. I will now try to \n");
-	g_print ("create a backtrace which you can attach to any support requests.\n\n");
-	g_on_error_stack_trace (PACKAGE);
-
-	_exit (1);
-}
-
 static void
 signal_handler (int sig)
 {
@@ -317,14 +300,9 @@ main (int argc, char *argv[])
 	signal (SIGINT, signal_handler);
 	signal (SIGHUP, signal_handler);
 
-#ifndef G_OS_WIN32
-	signal (SIGBUS, fatal_signal_handler);
-	signal (SIGSEGV, fatal_signal_handler);
-#endif
-
 	/* Note: we explicitely do not use the gdk_thread_*
 	   locking in Liferea because it freezes the program
-	   when running Flash applets in gtkmozembed */
+	   when running Flash applets */
 
 	runState = STATE_STARTING;
 	
