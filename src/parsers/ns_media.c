13b09aee142529b64a73c8e25b4b40d817f354b4
Lars Windolf <lars.lindner@gmail.com>
2012-08-14 00:31:39 +0200

    Improve indention.

--- src/parsers/ns_media.c
+++ src/parsers/ns_media.c
@@ -57,46 +57,47 @@ parse_item_tag (feedParserCtxtPtr ctxt, xmlNodePtr cur)
 		       
 	   (example quoted from specification)
 	*/
-  	if (!xmlStrcmp(cur->name, BAD_CAST"content")) {
+  	if (xmlStrcmp(cur->name, BAD_CAST"content")) {
 		tmp = xml_get_attribute (cur, "url");
-		if (tmp) {
-			/* the following code is duplicated from rss_item.c! */
-			const gchar *feedURL = subscription_get_homepage (ctxt->subscription);
-			
-			gchar *type = xml_get_attribute (cur, "type");
-			gchar *lengthStr = xml_get_attribute (cur, "length");
-			gchar *medium = xml_get_attribute (cur, "medium");
-			gssize length = 0;
-			if (lengthStr)
-				length = atol (lengthStr);
-			
-			if ((strstr (tmp, "://") == NULL) && feedURL && (feedURL[0] != '|') &&
-			    (strstr (feedURL, "://") != NULL)) {
-				/* add base URL if necessary and possible */
-				 tmp2 = g_strdup_printf ("%s/%s", feedURL, tmp);
-				 g_free (tmp);
-				 tmp = tmp2;
-			}
-			
-			/* gravatars are often supplied as media:content with medium='image'
-			   so we treat do not treat such occurences as enclosures */
-			if (medium && !strcmp (medium, "image") && strstr (tmp, "www.gravatar.com")) {
-				metadata_list_set (&(ctxt->item->metadata), "gravatar", tmp);
-			} else {
-				/* Never add enclosures for images already contained in the description */
-				if (!(ctxt->item->description && strstr (ctxt->item->description, tmp))) {
-					gchar *encl_string = enclosure_values_to_string (tmp, type, length, FALSE /* not yet downloaded */);
-					ctxt->item->metadata = metadata_list_append (ctxt->item->metadata, "enclosure", encl_string);
-					g_free (encl_string);
+		if (!tmp)
+			return;
 
-					ctxt->item->hasEnclosure = TRUE;
-				}
-			}
-			g_free (tmp);
-			g_free (type);
-			g_free (medium);
-			g_free (lengthStr);
+		/* the following code is duplicated from rss_item.c! */
+		const gchar *feedURL = subscription_get_homepage (ctxt->subscription);
+	
+		gchar *type = xml_get_attribute (cur, "type");
+		gchar *lengthStr = xml_get_attribute (cur, "length");
+		gchar *medium = xml_get_attribute (cur, "medium");
+		gssize length = 0;
+		if (lengthStr)
+			length = atol (lengthStr);
+	
+		if ((strstr (tmp, "://") == NULL) && feedURL && (feedURL[0] != '|') &&
+		    (strstr (feedURL, "://") != NULL)) {
+			/* add base URL if necessary and possible */
+			 tmp2 = g_strdup_printf ("%s/%s", feedURL, tmp);
+			 g_free (tmp);
+			 tmp = tmp2;
 		}
+	
+		/* gravatars are often supplied as media:content with medium='image'
+		   so we treat do not treat such occurences as enclosures */
+		if (medium && !strcmp (medium, "image") && strstr (tmp, "www.gravatar.com")) {
+			metadata_list_set (&(ctxt->item->metadata), "gravatar", tmp);
+		} else {
+			/* Never add enclosures for images already contained in the description */
+			if (!(ctxt->item->description && strstr (ctxt->item->description, tmp))) {
+				gchar *encl_string = enclosure_values_to_string (tmp, type, length, FALSE /* not yet downloaded */);
+				ctxt->item->metadata = metadata_list_append (ctxt->item->metadata, "enclosure", encl_string);
+				g_free (encl_string);
+
+				ctxt->item->hasEnclosure = TRUE;
+			}
+		}
+		g_free (tmp);
+		g_free (type);
+		g_free (medium);
+		g_free (lengthStr);
 	}
 	
 	// FIXME: should we support media:player too?
