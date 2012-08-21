f0c83ab4e617cb48d98e0f57c05d427816d34897
Lars Windolf <lars.lindner@gmail.com>
2012-08-21 19:12:10 +0200

    Isolating browser history code as a preparation to move the URI entry and the history buttons into LifereaHtmlView.

---
+++ src/browser_history.c
@@ -0,0 +1,106 @@
+/**
+ * @file browser_history.c  managing the URI history
+ *
+ * Copyright (C) 2012 Lars Windolf <lars.lindner@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#include "browser_history.h"
+
+browserHistory *
+browser_history_new (void)
+{	
+	return (browserHistory *)g_new0 (browserHistory, 1);
+}
+
+void
+browser_history_free (browserHistory *history)
+{
+	GList	*iter;
+
+	g_return_if_fail (NULL != history);
+	iter = history->locations;
+	while (iter) {
+		g_free (iter->data);
+		iter = g_list_next (iter);
+	}
+	g_list_free (history->locations);
+	g_free (history);
+}
+
+gchar *
+browser_history_forward (browserHistory *history)
+{
+	GList	*url = history->current;
+
+	url = g_list_next (url);
+	history->current = url;
+
+	return url->data;
+}
+
+gchar *
+browser_history_back (browserHistory *history)
+{
+	GList	*url = history->current;
+
+	url = g_list_previous (url);
+	history->current = url;
+
+	return url->data;
+}
+
+gboolean
+browser_history_can_go_forward (browserHistory *history)
+{
+	return (NULL != g_list_next (history->current));
+}
+
+gboolean
+browser_history_can_go_back (browserHistory *history)
+{
+	return (NULL != g_list_previous (history->current));
+}
+
+void
+browser_history_add_location (browserHistory *history, const gchar *url)
+{
+	GList 	*iter;
+
+	/* Do not add the same URL twice in a row... */
+	if (history->current &&
+	   g_str_equal (history->current->data, url))
+		return;
+
+	/* If current URL is not at the end of the list,
+	   truncate the rest of the list */
+	if (history->locations) {
+		while (1) {
+			iter = g_list_last (history->locations);
+			if (!iter)
+				break;
+			if (iter == history->current)
+				break;
+			g_free (iter->data);
+			history->locations = g_list_remove (history->locations, iter->data);
+		}
+	}
+
+	history->locations = g_list_append (history->locations, g_strdup (url));
+	history->current = g_list_last (history->locations);
+}
+
+
---
+++ src/browser_history.h
@@ -0,0 +1,83 @@
+/**
+ * @file browser_history.h  managing the URI history
+ *
+ * Copyright (C) 2012 Lars Windolf <lars.lindner@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA 
+ */
+
+#ifndef _BROWSER_HISTORY_H
+#define _BROWSER_HISTORY_H
+
+#include <glib.h>
+
+/** structure holding all URLs visited in a tab */
+typedef struct browserHistory {
+	GList		*locations;	/**< list of all visited URLs */
+	GList		*current;	/**< pointer into locations */
+} browserHistory;
+
+/**
+ * Create a new browser history.
+ *
+ * @returns the browser history
+ */
+browserHistory * browser_history_new (void);
+
+/**
+ * Dispose of a browser history
+ *
+ * @param history	the browser history
+ */
+void browser_history_free (browserHistory *history);
+
+/**
+ * Set index of the browser history forward.
+ *
+ * @param history	the browser history
+ *
+ * @returns the new selected URL (not to be free'd!)
+ */
+gchar * browser_history_forward (browserHistory *history);
+
+/**
+ * Set index of the browser history backwards.
+ *
+ * @param history	the browser history
+ *
+ * @returns the new selected URL (not to be free'd!)
+ */
+gchar * browser_history_back (browserHistory *history);
+
+/**
+ * Check wether the history can go forward.
+ *
+ * @param history	the browser history
+ *
+ * @returns TRUE if it can go forward
+ */
+gboolean browser_history_can_go_forward (browserHistory *history);
+
+/**
+ * Check wether the history can go back.
+ *
+ * @param history	the browser history
+ *
+ * @returns TRUE if it can go back
+ */
+gboolean browser_history_can_go_back (browserHistory *history);
+
+#endif
+
