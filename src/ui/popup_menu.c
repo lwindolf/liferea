7b3b21ad013dc6f970d87b3009026d56252beabf
Adrian Bunk <bunk@users.sourceforge.net>
2013-04-29 17:19:37 +0300

    ui_popup_node_menu(): Explicitly enable addChildren for the root node
    Previously the value of addChildren was semi-random (the variable
    was never initizlized).

--- src/ui/popup_menu.c
+++ src/ui/popup_menu.c
@@ -289,6 +289,7 @@ ui_popup_node_menu (nodePtr node, gboolean validSelection, guint button, guint32
 		writeableFeedlist = TRUE;
 		isRoot = TRUE;
 		isHierarchic = TRUE;
+		addChildren = TRUE;
 	}
 
 	if (validSelection) {
