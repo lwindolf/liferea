b0b3a48d3509ee3c91f21b7bda53b0c554984a9d
Lars Windolf <lars.lindner@gmail.com>
2013-07-08 23:11:09 +0200

    Fixes mistake with command checking

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
-autoreconf -i || echo "ERROR: You need to install autoconf!" && exit 1
-intltoolize || echo "ERROR: You need to install intltool!" && exit 1
+tmp=`which autoreconf`
+if [ "$tmp" = "" ]; then
+	echo "ERROR: You need to install autoconf!"
+	exit 1
+fi
+
+tmp=`which intltoolize`
+if [ "$tmp" = "" ]; then
+	echo "ERROR: You need to install intltool!"
+	exit 1
+fi
+
+autoreconf -i
+intltoolize
 ./configure "$@"
 
