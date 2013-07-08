f1a53b882fff2b52e8e6b65476c4898060c41019
Lars Windolf <lars.lindner@gmail.com>
2013-07-08 23:02:29 +0200

    Print some errors on missing autoconf or intltool

--- autogen.sh
+++ autogen.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-autoreconf -i
-intltoolize
+autoreconf -i || echo "ERROR: You need to install autoconf!" && exit 1
+intltoolize || echo "ERROR: You need to install intltool!" && exit 1
 ./configure "$@"
 
