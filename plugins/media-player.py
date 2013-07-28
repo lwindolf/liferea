fd7bc643ada8d39aed1af1c1ec556d666bd934f5
Lars Windolf <lars.lindner@gmail.com>
2013-07-28 21:56:59 +0200

    Fix indentation and move time label formatting into a function (Simon Kågedal Reimer)

--- plugins/media-player.py
+++ plugins/media-player.py
@@ -36,45 +36,51 @@ class MediaPlayerPlugin(GObject.Object, Liferea.MediaPlayerActivatable):
         self.player.set_state(Gst.State.NULL)
         self.playing = False
         self.slider.set_value(0)
-        self.label.set_text("0:00")
+        self.set_label(0)
         self.updateButtons()
 
     def on_eos(self, bus, message):
         self.set_stopped()
 
     def play(self):
-	uri = Liferea.enclosure_get_url(self.enclosures[self.pos])
-	self.player.set_property("uri", uri)
-	self.player.set_state(Gst.State.PLAYING)
-	Liferea.ItemView.select_enclosure(self.pos)
-	GObject.timeout_add(1000, self.updateSlider)
+        uri = Liferea.enclosure_get_url(self.enclosures[self.pos])
+        self.player.set_property("uri", uri)
+        self.player.set_state(Gst.State.PLAYING)
+        Liferea.ItemView.select_enclosure(self.pos)
+        GObject.timeout_add(1000, self.updateSlider)
 
     def stop(self):
-	self.player.set_state(Gst.State.NULL)
-	
+        self.player.set_state(Gst.State.NULL)
+        
     def playToggled(self, w):
         self.slider.set_value(0)
-	self.label.set_text("0:00")
+        self.set_label(0)
 
-	if(self.playing == False):
-		self.play()
-	else:
-		self.stop()
+        if(self.playing == False):
+                self.play()
+        else:
+                self.stop()
 
-	self.playing=not(self.playing)
-	self.updateButtons()
+        self.playing=not(self.playing)
+        self.updateButtons()
 
     def next(self, w):
-	self.stop()
+        self.stop()
         self.pos+=1
-	self.play()
-	self.updateButtons()
+        self.play()
+        self.updateButtons()
 
     def prev(self, w):
-	self.stop()
+        self.stop()
         self.pos-=1
-	self.play()
-	self.updateButtons()       
+        self.play()
+        self.updateButtons()       
+
+    def set_label(self, position):
+        format = "%d:%02d"
+        #if self.moving_slider:
+        #    format = "<i>%s</i>" % format
+        self.label.set_markup (format % (position / 60, position % 60))
 
     def updateSlider(self):
 	if(self.playing == False):
@@ -105,11 +111,11 @@ class MediaPlayerPlugin(GObject.Object, Liferea.MediaPlayerActivatable):
 		print e
 		pass
 
-	return True
+        return True
 
     def updateButtons(self):
         Gtk.Widget.set_sensitive(self.prevButton, (self.pos != 0))
-	Gtk.Widget.set_sensitive(self.nextButton, (len(self.enclosures) - 1 > self.pos))
+        Gtk.Widget.set_sensitive(self.nextButton, (len(self.enclosures) - 1 > self.pos))
 
         if(self.playing == False):
            self.playButtonImage.set_from_stock("gtk-media-play", Gtk.IconSize.BUTTON)
@@ -123,57 +129,58 @@ class MediaPlayerPlugin(GObject.Object, Liferea.MediaPlayerActivatable):
         # Test whether Media Player widget already exists
         childList = Gtk.Container.get_children(parentWidget)
 
-	if len(childList) == 1:
-	   # We need to add a media player...
-	   vbox = Gtk.Box(Gtk.Orientation.HORIZONTAL, 0)
-	   vbox.set_margin_top(3)
-	   vbox.set_margin_bottom(3)
-	   Gtk.Box.pack_start(parentWidget, vbox, True, True, 0);
+        if len(childList) == 1:
+           # We need to add a media player...
+           vbox = Gtk.Box(Gtk.Orientation.HORIZONTAL, 0)
+           vbox.set_margin_top(3)
+           vbox.set_margin_bottom(3)
+           Gtk.Box.pack_start(parentWidget, vbox, True, True, 0);
 
-	   image = Gtk.Image()
+           image = Gtk.Image()
            image.set_from_stock("gtk-media-previous", Gtk.IconSize.BUTTON)
            self.prevButton = Gtk.Button.new()
            self.prevButton.add(image)
            self.prevButton.connect("clicked", self.prev)
-	   Gtk.Box.pack_start(vbox, self.prevButton, False, False, 0)
+           Gtk.Box.pack_start(vbox, self.prevButton, False, False, 0)
 
            self.playButtonImage = Gtk.Image()
            self.playButtonImage.set_from_stock("gtk-media-play", Gtk.IconSize.BUTTON)
            self.playButton = Gtk.Button.new()
            self.playButton.add(self.playButtonImage)
            self.playButton.connect("clicked", self.playToggled)
-	   Gtk.Box.pack_start(vbox, self.playButton, False, False, 0)
+           Gtk.Box.pack_start(vbox, self.playButton, False, False, 0)
 
-	   image = Gtk.Image()
+           image = Gtk.Image()
            image.set_from_stock("gtk-media-next", Gtk.IconSize.BUTTON)
            self.nextButton = Gtk.Button.new()
            self.nextButton.add(image)
            self.nextButton.connect("clicked", self.next)
-	   Gtk.Box.pack_start(vbox, self.nextButton, False, False, 0)
+           Gtk.Box.pack_start(vbox, self.nextButton, False, False, 0)
 
-	   self.slider = Gtk.Scale(orientation = Gtk.Orientation.HORIZONTAL)
-	   self.slider.set_margin_left(6)
-	   self.slider.set_margin_right(6)
+           self.slider = Gtk.Scale(orientation = Gtk.Orientation.HORIZONTAL)
+           self.slider.set_margin_left(6)
+           self.slider.set_margin_right(6)
            self.slider.set_draw_value(False)
-	   self.slider.set_range(0, 100)
-	   self.slider.set_increments(1, 10)
+           self.slider.set_range(0, 100)
+           self.slider.set_increments(1, 10)
 
-	   Gtk.Box.pack_start(vbox, self.slider, True, True, 0)
+           Gtk.Box.pack_start(vbox, self.slider, True, True, 0)
 
-	   self.label = Gtk.Label(label='0:00')
-	   self.label.set_margin_left(6)
-	   self.label.set_margin_right(6)
-	   Gtk.Box.pack_start(vbox, self.label, False, False, 0)
+           self.label = Gtk.Label()
+           self.set_label(0)
+           self.label.set_margin_left(6)
+           self.label.set_margin_right(6)
+           Gtk.Box.pack_start(vbox, self.label, False, False, 0)
 
-	   Gtk.Widget.show_all(vbox)
+           Gtk.Widget.show_all(vbox)
 
-	self.enclosures = enclosures
-	self.pos = 0
+        self.enclosures = enclosures
+        self.pos = 0
         # FIXME: make start on entry configurable
         self.set_stopped()        
 
     #def do_activate(self):
-	#print "=== MediaPlayer activate"
+        #print("=== MediaPlayer activate")
 
     def do_deactivate(self):
         window = self.object
