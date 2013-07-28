f5a39a76cce24059beaa293689785b865dd5cf81
Lars Windolf <lars.lindner@gmail.com>
2013-07-28 22:15:11 +0200

    Allow dragging media player slider for seeking (Simon Kågedal Reimer)

--- plugins/media-player.py
+++ plugins/media-player.py
@@ -1,3 +1,6 @@
+# Make code still work under Python 2.6/2.7
+from __future__ import print_function, division
+
 from gi.repository import GObject
 from gi.repository import Peas
 from gi.repository import PeasGtk
@@ -16,31 +19,34 @@ class MediaPlayerPlugin(GObject.Object, Liferea.MediaPlayerActivatable):
         self.IS_GST010 = Gst.version() < (0, 11)
 
         playbin = "playbin2" if self.IS_GST010 else "playbin"
-	self.player = Gst.ElementFactory.make(playbin, "player")
+        self.player = Gst.ElementFactory.make(playbin, "player")
         if not self.IS_GST010:
             fakesink = Gst.ElementFactory.make("fakesink", "fakesink")
             self.player.set_property("video-sink", fakesink)
             bus = self.player.get_bus()
             bus.add_signal_watch()
             bus.connect("message::eos", self.on_eos)
-            bus.connect('message::error', self.on_error)
+            bus.connect("message::error", self.on_error)
+
+        self.moving_slider = False
 
     def on_error(self, bus, message):
+        self.player.set_state(Gst.State.NULL)
         err, debug = message.parse_error()
         print("Error: %s" % err, debug)
+        self.playing = False
+        self.updateButtons()
+
+    def on_eos(self, bus, message):
         self.player.set_state(Gst.State.NULL)
         self.playing = False
         self.updateButtons()
- 
-    def set_stopped(self):
-        self.player.set_state(Gst.State.NULL)
+
+    def on_finished(self, player):
         self.playing = False
         self.slider.set_value(0)
         self.set_label(0)
         self.updateButtons()
-
-    def on_eos(self, bus, message):
-        self.set_stopped()
 
     def play(self):
         uri = Liferea.enclosure_get_url(self.enclosures[self.pos])
@@ -78,38 +84,43 @@ class MediaPlayerPlugin(GObject.Object, Liferea.MediaPlayerActivatable):
 
     def set_label(self, position):
         format = "%d:%02d"
-        #if self.moving_slider:
-        #    format = "<i>%s</i>" % format
+        if self.moving_slider:
+            format = "<i>%s</i>" % format
         self.label.set_markup (format % (position / 60, position % 60))
 
+    def get_player_position(self):
+        """Get the GStreamer player's position in nanoseconds"""
+        try:
+           if self.IS_GST010:
+              return self.player.query_position(Gst.Format.TIME)[2]
+           else:
+              return self.player.query_position(Gst.Format.TIME)[1]
+        except Exception as e:
+            # pipeline must not be ready and does not know position
+            print(e)
+            return 0
+
+    def get_player_duration(self):
+        """Get the GStreamer player's duration in nanoseconds"""
+        try:
+            if self.IS_GST010:
+                return self.player.query_duration(Gst.Format.TIME)[2]
+            else:
+                return self.player.query_duration(Gst.Format.TIME)[1]
+        except Exception as e:
+            # pipeline must not be ready and does not know position
+            print(e)
+            return 0
+
     def updateSlider(self):
-	if(self.playing == False):
-	   return False # cancel timeout
+        if not self.playing or self.moving_slider:
+            return False # cancel timeout
 
-	try:
-	   if self.IS_GST010:
-	      nanosecs = self.player.query_position(Gst.Format.TIME)[2]
-	      duration_nanosecs = self.player.query_duration(Gst.Format.TIME)[2]
-	   else:
-	      nanosecs = self.player.query_position(Gst.Format.TIME)[1]
-	      duration_nanosecs = self.player.query_duration(Gst.Format.TIME)[1]
-
-	   # FIXME!
-	   # block seek handler so we don't seek when we set_value()
-	   # self.slider.handler_block_by_func(self.on_slider_change)
-
-           duration = float(duration_nanosecs) / Gst.SECOND
-	   position = float(nanosecs) / Gst.SECOND
-	   self.slider.set_range(0, duration)
-	   self.slider.set_value(position)
-           self.label.set_text ("%d" % (position / 60) + ":%02d" % (position % 60))
-
-	   #self.slider.handler_unblock_by_func(self.on_slider_change)
-	
-	except Exception as e:
-		# pipeline must not be ready and does not know position
-		print e
-		pass
+        duration = self.get_player_duration() / Gst.SECOND
+        position = self.get_player_position() / Gst.SECOND
+        self.slider.set_range(0, duration)
+        self.slider.set_value(position)
+        self.set_label(position)
 
         return True
 
@@ -122,9 +133,31 @@ class MediaPlayerPlugin(GObject.Object, Liferea.MediaPlayerActivatable):
         else:
            self.playButtonImage.set_from_stock("gtk-media-stop", Gtk.IconSize.BUTTON)
 
+    def on_slider_change_value(self, widget, scroll, value):
+        self.set_label(value) 
+        if not self.moving_slider:
+            nanosecs = value * Gst.SECOND
+            # Stop when moving slider to very near the end
+            end_cutoff = self.get_player_duration() - Gst.SECOND
+            if nanosecs > end_cutoff and self.playing:
+                self.playToggled(None)
+            else:
+                self.player.seek_simple(Gst.Format.TIME,
+                                        Gst.SeekFlags.FLUSH | 
+                                        Gst.SeekFlags.KEY_UNIT,
+                                        nanosecs)
+
+        return False
+
+    def on_slider_button_press(self, widget, event):
+        self.moving_slider = True
+
+    def on_slider_button_release(self, widget, event):
+        self.moving_slider = False
+
     def do_load(self, parentWidget, enclosures):
         if parentWidget == None:
-           print "ERROR: Could not find media player insertion widget!"
+           print("ERROR: Could not find media player insertion widget!")
 
         # Test whether Media Player widget already exists
         childList = Gtk.Container.get_children(parentWidget)
@@ -163,6 +196,11 @@ class MediaPlayerPlugin(GObject.Object, Liferea.MediaPlayerActivatable):
            self.slider.set_draw_value(False)
            self.slider.set_range(0, 100)
            self.slider.set_increments(1, 10)
+           self.slider.connect("change-value", self.on_slider_change_value)
+           self.slider.connect("button-press-event",
+                               self.on_slider_button_press)
+           self.slider.connect("button-release-event", 
+                               self.on_slider_button_release)
 
            Gtk.Box.pack_start(vbox, self.slider, True, True, 0)
 
@@ -176,8 +214,8 @@ class MediaPlayerPlugin(GObject.Object, Liferea.MediaPlayerActivatable):
 
         self.enclosures = enclosures
         self.pos = 0
-        # FIXME: make start on entry configurable
-        self.set_stopped()        
+        self.player.set_state(Gst.State.NULL)   # FIXME: Make this configurable?
+        self.on_finished(self.player)
 
     #def do_activate(self):
         #print("=== MediaPlayer activate")
