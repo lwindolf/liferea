d7e2e6c1dc1320f24f2697b02b4cad2b638ed917
Lars Windolf <lars.lindner@gmail.com>
2013-12-28 23:42:28 +0100

    Make window toggling work

---
+++ plugins/trayicon.py
@@ -0,0 +1,34 @@
+from gi.repository import GObject
+from gi.repository import Peas
+from gi.repository import PeasGtk
+from gi.repository import Gtk
+from gi.repository import Liferea
+
+class TrayiconPlugin (GObject.Object, Liferea.ShellActivatable):
+    __gtype_name__ = 'TrayiconPlugin'
+
+    object = GObject.property (type=GObject.Object)
+    shell = GObject.property (type=Liferea.Shell)
+
+    def do_activate (self):
+        self.staticon = Gtk.StatusIcon ()
+	#self.icon.set_from_file(self.myicon)
+        self.staticon.set_from_stock (Gtk.STOCK_ABOUT)
+        self.staticon.connect ("activate", self.trayicon_activate)
+        self.staticon.connect ("popup_menu", self.trayicon_popup)
+        self.staticon.set_visible (True)
+
+    def trayicon_activate (self, widget, data = None):
+	window = self.shell.get_window ()
+	state = Gtk.Widget.get_visible (window)
+	if True == state:
+		Gtk.Widget.hide (window)
+	else:
+		Gtk.Window.present (window)
+
+    def trayicon_popup (self, widget, button, time, data = None):
+        print "popup menu triggered"
+
+    def do_deactivate (self):
+        self.staticon.set_visible (False)
+        del self.staticon
