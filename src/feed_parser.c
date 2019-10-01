daa6760a2619aace8e3b21ec4f5d160c6caa6104
Lars Windolf <lars.windolf@gmx.de>
2019-10-01 20:42:30 +0200

    Drop Atom 0.3 (aka Pie) and CDF channel support.

--- src/feed_parser.c
+++ src/feed_parser.c
@@ -1,12 +1,12 @@
 /**
  * @file feed_parser.c  parsing of different feed formats
- * 
- * Copyright (C) 2008-2017 Lars Windolf <lars.windolf@gmx.de>
+ *
+ * Copyright (C) 2008-2019 Lars Windolf <lars.windolf@gmx.de>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version. 
+ * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
- 
+
 #include <string.h>
 
 #include "common.h"
@@ -25,10 +25,8 @@
 #include "html.h"
 #include "metadata.h"
 #include "xml.h"
-#include "parsers/cdf_channel.h"
 #include "parsers/rss_channel.h"
 #include "parsers/atom10.h"
-#include "parsers/pie_feed.h"
 
 static GSList *feedHandlers = NULL;	/**< list of available parser implementations */
 
@@ -42,12 +40,10 @@ feed_parsers_get_list (void)
 {
 	if (feedHandlers)
 		return feedHandlers;
-	
+
 	feedHandlers = g_slist_append (feedHandlers, rss_init_feed_handler ());
-	feedHandlers = g_slist_append (feedHandlers, cdf_init_feed_handler ());
-	feedHandlers = g_slist_append (feedHandlers, atom10_init_feed_handler ());  /* Must be before pie */
-	feedHandlers = g_slist_append (feedHandlers, pie_init_feed_handler ());
-	
+	feedHandlers = g_slist_append (feedHandlers, atom10_init_feed_handler ());
+
 	return feedHandlers;
 }
 
@@ -64,12 +60,9 @@ feed_type_str_to_fhp (const gchar *str)
 {
 	GSList *iter;
 	feedHandlerPtr fhp = NULL;
-	
+
 	if (!str)
 		return NULL;
-		
-	if (strstr(str, "pie"))
-		return feed_type_str_to_fhp ("atom");
 
 	for(iter = feed_parsers_get_list (); iter != NULL; iter = iter->next) {
 		fhp = (feedHandlerPtr)iter->data;
@@ -140,11 +133,11 @@ feed_parser_auto_discover (feedParserCtxtPtr ctxt)
 
 /**
  * General feed source parsing function. Parses the passed feed source
- * and tries to determine the source type. 
+ * and tries to determine the source type.
  *
  * @param ctxt		feed parsing context
  *
- * @returns FALSE if auto discovery is indicated, 
+ * @returns FALSE if auto discovery is indicated,
  *          TRUE if feed type was recognized and parsing was successful
  */
 gboolean
@@ -156,7 +149,7 @@ feed_parse (feedParserCtxtPtr ctxt)
 	debug_enter("feed_parse");
 
 	g_assert(NULL == ctxt->items);
-	
+
 	ctxt->failed = TRUE;	/* reset on success ... */
 
 	if(ctxt->feed->parseErrors)
@@ -164,25 +157,25 @@ feed_parse (feedParserCtxtPtr ctxt)
 	else
 		ctxt->feed->parseErrors = g_string_new(NULL);
 
-	/* try to parse buffer with XML and to create a DOM tree */	
+	/* try to parse buffer with XML and to create a DOM tree */
 	do {
 		if(NULL == xml_parse_feed (ctxt)) {
 			g_string_append_printf (ctxt->feed->parseErrors, _("XML error while reading feed! Feed \"%s\" could not be loaded!"), subscription_get_source (ctxt->subscription));
 			break;
 		}
-		
+
 		if(NULL == (cur = xmlDocGetRootElement(ctxt->doc))) {
 			g_string_append(ctxt->feed->parseErrors, _("Empty document!"));
 			break;
 		}
-		
+
 		while(cur && xmlIsBlankNode(cur)) {
 			cur = cur->next;
 		}
-		
+
 		if(!cur)
 			break;
-				
+
 		if(!cur->name) {
 			g_string_append(ctxt->feed->parseErrors, _("Invalid XML!"));
 			break;
@@ -194,12 +187,12 @@ feed_parse (feedParserCtxtPtr ctxt)
 			feedHandlerPtr handler = (feedHandlerPtr)(handlerIter->data);
 			if(handler && handler->checkFormat && (*(handler->checkFormat))(ctxt->doc, cur)) {
 				/* free old temp. parsing data, don't free right after parsing because
-				   it can be used until the last feed request is finished, move me 
-				   to the place where the last request in list otherRequests is 
+				   it can be used until the last feed request is finished, move me
+				   to the place where the last request in list otherRequests is
 				   finished :-) */
 				g_hash_table_destroy(ctxt->tmpdata);
 				ctxt->tmpdata = g_hash_table_new_full(g_str_hash, g_str_equal, NULL, g_free);
-				
+
 				/* we always drop old metadata */
 				metadata_list_free(ctxt->subscription->metadata);
 				ctxt->subscription->metadata = NULL;
@@ -233,13 +226,13 @@ feed_parse (feedParserCtxtPtr ctxt)
 		debug1(DEBUG_UPDATE, "discovered feed format: %s", feed_type_fhp_to_str(ctxt->feed->fhp));
 		success = TRUE;
 	}
-	
+
 	if(ctxt->doc) {
 		xmlFreeDoc(ctxt->doc);
 		ctxt->doc = NULL;
 	}
-		
+
 	debug_exit("feed_parse");
-	
+
 	return success;
 }
