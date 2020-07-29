f7cbb56daf07eb8e224d2d61576bdbace3f003e7
Lars Windolf <lars.windolf@gmx.de>
2020-07-30 00:27:08 +0200

    Improve error output handling. Always show warnings.

--- src/tests/test_a11y.sh
+++ src/tests/test_a11y.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 if command -vp gla11y >/dev/null; then
-	if gla11y ../../glade/*.ui | grep FATAL; then
+	output=$(gla11y ../../glade/*.ui)
+	echo "$output"
+
+	# For now lets prevent only fatals
+	if echo "$output" | grep -q FATAL; then
 		printf "ERROR: Fatal accessibility issues were found!\n"
 		exit 1
 	else
