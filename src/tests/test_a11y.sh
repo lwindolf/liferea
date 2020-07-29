5420fd5e8d09d4662f5cceee33aad8b25f30693d
Lars Windolf <lars.windolf@gmx.de>
2020-07-30 00:21:33 +0200

    Fix gla11y check and error messages.

--- src/tests/test_a11y.sh
+++ src/tests/test_a11y.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 if command -vp gla11y >/dev/null; then
-	gla11y ../../glade/*.ui | grep FATAL && exit 1
+	if gla11y ../../glade/*.ui | grep FATAL; then
+		printf "ERROR: Fatal accessibility issues were found!\n"
+		exit 1
+	else
+		printf "Accessibility looks fine.\n"
+	fi
 else
-	printf "WARNING: gla11y is not installed, cannot test accessibility"
+	printf "WARNING: gla11y is not installed, cannot test accessibility\n"
 fi
