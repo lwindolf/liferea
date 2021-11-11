3812b9a02f4fbc3f7fb89c13101ab4edae390b93
Paweł Marciniak <sunwire+git@gmail.com>
2021-11-11 15:16:10 +0100

    use g_time_zone_new_identifier only when it is available

--- src/date.c
+++ src/date.c
@@ -342,7 +342,11 @@ date_parse_rfc822_tz (char *token)
 	int num_timezones = sizeof (tz_offsets) / sizeof ((tz_offsets)[0]);
 
 	if (*inptr == '+' || *inptr == '-') {
-		return g_time_zone_new_identifier (inptr);
+		#ifdef HAVE_G_TIME_ZONE_NEW_IDENTIFIER
+			return g_time_zone_new_identifier (inptr);
+		#else
+			return g_time_zone_new (inptr);
+		#endif
 	} else {
 		int t;
 
@@ -351,7 +355,11 @@ date_parse_rfc822_tz (char *token)
 
 		for (t = 0; t < num_timezones; t++)
 			if (!strncmp (inptr, tz_offsets[t].name, strlen (tz_offsets[t].name)))
-				return g_time_zone_new_identifier (tz_offsets[t].offset);
+				#ifdef HAVE_G_TIME_ZONE_NEW_IDENTIFIER
+					return g_time_zone_new_identifier (tz_offsets[t].offset);
+				#else
+					return g_time_zone_new (tz_offsets[t].offset);
+				#endif
 	}
 
 	return NULL;
