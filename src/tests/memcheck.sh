a4335843888f69e7b78d1ff7c1d7c2ab2fdcebea
Lars Windolf <lars.windolf@gmx.de>
2021-11-12 23:30:02 +0100

    Update memcheck.sh

--- src/tests/memcheck.sh
+++ src/tests/memcheck.sh
@@ -8,8 +8,11 @@ error=0
 
 if command -v valgrind >/dev/null; then
 	for tool in $@; do
+		details=$(
+			valgrind -q --leak-check=full --error-markers=begin,end "./$tool" 2>&1#
+		)
 		output=$(
-			valgrind -q --leak-check=full --error-markers=begin,end "./$tool" 2>&1 |\
+			echo "$details" |\
 			grep -A1 "== begin" |\
 			egrep -v "== begin|possibly lost|^--$"
 		)
@@ -17,6 +20,13 @@ if command -v valgrind >/dev/null; then
 			error=1
 			echo "ERROR: memcheck reports problems for '$tool'!"
 			echo "$output"
+			
+			# When in github provide extra details
+			if [ "$GITHUB_ACTION" != "" ]; then
+				echo "::group:: $tool details"
+				echo "$details"
+				echo "::endgroup:"
+			fi
 		fi
 	done
 else
