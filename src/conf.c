f4bf81f4b3f821b4dd5ccdc0c26cfc59bd4a84cd
Lars Windolf <lars.windolf@gmx.de>
2023-03-31 19:38:04 +0200

    Fix g_object_unref warning on shutdown

--- src/conf.c
+++ src/conf.c
@@ -388,6 +388,7 @@ conf_deinit (void)
 {
 	g_object_unref (settings);
 	g_object_unref (desktop_settings);
-	g_object_unref (fdo_settings);
+	if (fdo_settings)
+		g_object_unref (fdo_settings);
 }
 
