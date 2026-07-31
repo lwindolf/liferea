32077a78a76c5dfc85fb16ac846acbf64a32f882
Lars Windolf <lars.windolf@gmx.de>
2026-07-31 23:12:32 +0200

    Fix location of itemlist popup menu

--- src/ui/item_list_view.c
+++ src/ui/item_list_view.c
@@ -793,11 +793,24 @@ on_item_list_view_pressed_event (GtkGestureClick *gesture, guint n_press, gdoubl
 			case GDK_BUTTON_SECONDARY: {
 				GMenu *menu = item_list_view_popup_menu (ilv, item);
 				GtkWidget *popover = gtk_popover_menu_new_from_model (G_MENU_MODEL (menu));
+				GtkWidget *anchor = gtk_widget_get_parent (GTK_WIDGET (ilv->listbox));
 				GdkRectangle rect;
+				graphene_point_t src = GRAPHENE_POINT_INIT ((float)x, (float)y);
+				graphene_point_t dst;
 
-				gtk_widget_set_parent (popover, ilv->ilscrolledwindow);
-				rect.x = (int)x;
-				rect.y = (int)y;
+				if (!anchor)
+					anchor = GTK_WIDGET (ilv->listbox);
+
+				gtk_widget_set_parent (popover, anchor);
+
+				if (anchor != GTK_WIDGET (ilv->listbox) &&
+				    gtk_widget_compute_point (GTK_WIDGET (ilv->listbox), anchor, &src, &dst)) {
+					rect.x = (int)dst.x;
+					rect.y = (int)dst.y;
+				} else {
+					rect.x = (int)x;
+					rect.y = (int)y;
+				}
 				rect.width = 1;
 				rect.height = 1;
 				gtk_popover_set_pointing_to (GTK_POPOVER (popover), &rect);
