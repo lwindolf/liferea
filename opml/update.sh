fb2920685d0680961384a645a1a1eb11f4b76eef
Lars Windolf <lars.windolf@gmx.de>
2026-09-01 22:16:37 +0200

    Update and fix OPML

--- opml/update.sh
+++ opml/update.sh
@@ -13,7 +13,7 @@ sed -n "/$MATCH_START/,/$MATCH_END/p" feedlist_en.opml >$TMPFILE
 
 OPML_FILES=$(ls *.opml | grep -v "feedlist_en.opml")
 for f in $OPML_FILES; do
-    echo "Processing $f"
+    echo "Udpating $f"
     sed -i "/$MATCH_START/,/$MATCH_END/{
         /$MATCH_START/{
             r $TMPFILE
@@ -21,3 +21,53 @@ for f in $OPML_FILES; do
         d
     }" "$f"
 done
+
+echo "Linting OPML files..."
+for f in $OPML_FILES; do
+    echo "Linting $f"
+    xmllint --noout "$f"
+done
+
+echo "Testing URLs in OPML files..."
+
+declare -A already_tested
+test_url() {
+        url=$1
+        if [ "${already_tested[$url]}" != "" ]; then
+                if [ "${already_tested[$url]}" != "ok" ]; then
+                        echo "- (cached) HTTP ${already_tested[$url]} $url"
+                else
+                        echo "- (cached) OK $url"
+                fi
+                return
+        fi
+
+        if [ "$url" == "vfolder" ]; then
+                return
+        fi
+
+        status=$(curl -s --user-agent "Liferea/2.0 (Android 14; Mobile; https://lzone.de/liferea/) AppleWebKit (KHTML, like Gecko)" -o /dev/null -w "%{http_code}" "$url")
+        if ! [[ $status =~ ^[23][0-9][0-9]$ ]]; then
+                echo "- HTTP $status $url"
+                already_tested[$url]=$status
+        else
+                already_tested[$url]="ok"
+        fi
+        
+}
+
+# Extract all URLs and test them
+for f in $OPML_FILES; do
+    echo "Checking URLs in $f"
+    
+    grep -oP 'htmlUrl="\K[^"]+' "$f" | while read -r url; do
+        test_url "$url"
+    done
+
+    grep -oP 'xmlUrl="\K[^"]+' "$f" | while read -r url; do
+        test_url "$url"
+    done
+done
+
+rm "$TMPFILE"
+echo "Tests complete."
