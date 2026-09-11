fb353ccc9be0e223f385c05236466d11a8f2fa44
Lars Windolf <lars.windolf@gmx.de>
2026-09-11 17:02:02 +0200

    Provide already installed gsettings instead of compiling them for each test

--- src/tests/run.sh
+++ src/tests/run.sh
@@ -11,20 +11,15 @@ trap 'rm -rf "$tmp"' EXIT
 
 mkdir -p "$tmp/config/dconf"
 mkdir -p "$tmp/data"
-mkdir -p "$tmp/schema"
 
 # A private dconf profile.
 cat >"$tmp/dconf-profile" <<EOF
 user-db:test
 EOF
 
-cp "$(dirname "$0")/../../data"/*.gschema.xml "$tmp/schema"
-glib-compile-schemas "$tmp/schema"
-
 dbus-run-session -- env \
     DCONF_PROFILE="$tmp/dconf-profile" \
     XDG_CONFIG_HOME="$tmp/config" \
     XDG_DATA_HOME="$tmp/data" \
     GSETTINGS_BACKEND=dconf \
-    GSETTINGS_SCHEMA_DIR="$tmp/schema" \
     $CMD
